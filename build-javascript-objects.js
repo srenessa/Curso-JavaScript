2885373a5ce1c9d9d9cfd4ec65d705e49d72c112
Renessa Cerqueira <renessasantos123@gmail.com>
2022-10-11 09:52:05 -0300

    Construir objetos JavaScript

---
+++ build-javascript-objects.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+  name: "Lilly",
+  legs: 4,
+  tails: 1,
+  friends: ["humanos", "Perola"]
+  
+    // Only change code above this line
+  };
