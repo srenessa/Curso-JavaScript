f316dece127d1b7a1aa3181d803690759bcd0e25
Renessa Cerqueira <renessasantos123@gmail.com>
2022-10-13 10:32:39 -0300

    Manipulando Objetos Complexos

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,21 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+  {
+    artist: "Lilly",
+    title: "Lilly e Pell",
+    release_year: 2022,
+    formats:["CD", "My"]
+  }
+  
+  
+  ];
