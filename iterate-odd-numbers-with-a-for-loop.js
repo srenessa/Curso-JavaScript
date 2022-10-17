694005cabe23438571b6eacd841050266f2f17b8
Renessa Cerqueira <renessasantos123@gmail.com>
2022-10-17 09:43:20 -0300

    Iterar números ímpares com um loop for

--- iterate-odd-numbers-with-a-for-loop.js
+++ iterate-odd-numbers-with-a-for-loop.js
@@ -1,4 +1,7 @@
-// Setup
+//Iterar números ímpares com um loop for
 const myArray = [];
-
+for(let i = 1; i<10; i+=2){
+myArray.push(i);
+}
 // Only change code below this line
+console.log(myArray)
