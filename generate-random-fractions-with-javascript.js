e1ed6174a48e6edea93bc32e7b4f08cb355cf6a1
Renessa Cerqueira <renessasantos123@gmail.com>
2022-10-21 09:52:50 -0300

    Gerar frações aleatórias com JavaScript

---
+++ generate-random-fractions-with-javascript.js
@@ -0,0 +1,10 @@
+function randomFraction() {
+
+    // JavaScript tem uma função Math.random() que gera um número decimal aleatório entre 0 (inclusive) e 1 (exclusivo).
+    let value = 0;
+    while (value === 0) {
+
+        return Math.random();
+    }
+
+}
