bd1cb8e19aa121674b46941105e53f59e2f34955
Renessa Cerqueira <renessasantos123@gmail.com>
2022-10-21 10:27:15 -0300

    Gerar números inteiros aleatórios com JavaScript

--- generate-random-whole-numbers-with-javascript.js
+++ generate-random-whole-numbers-with-javascript.js
@@ -1,5 +1,8 @@
+//Estamos chamando Math.random(), multiplicando o resultado por 10, então passando o valor para a função Math.floor() para arredondar o valor para o número inteiro mais próximo.gerar. 
+
+//Estamos retornando um número inteiro aleatório entre 0 e 9.
 function randomWholeNum() {
 
-    return Math.floor(Math.random()*10);
-  }
-  console.log(randomWholeNum());
+    return Math.floor(Math.random() * 10);
+}
+console.log(randomWholeNum());
