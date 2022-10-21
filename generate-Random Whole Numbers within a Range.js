c80746d23d36752b4f0b97bea2759faf8aab322f
Renessa Cerqueira <renessasantos123@gmail.com>
2022-10-21 11:12:37 -0300

    Gerar números inteiros aleatórios dentro de um intervalo

--- generate-Random Whole Numbers within a Range.js
+++ generate-Random Whole Numbers within a Range.js
@@ -1,3 +1,4 @@
+//retorne um número inteiro aleatório maior ou igual a myMin e menor ou igual a myMax, inclusive.
 function randomRange(myMin, myMax) {
 
     return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
