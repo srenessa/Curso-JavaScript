0dfbb03596ea67c2f6bea84484c1c164f47a555d
Renessa Cerqueira <renessasantos123@gmail.com>
2022-11-04 18:19:10 -0300

    Use a atribuição de desestruturação para atribuir variáveis ​​de objetos

--- ES6/Use Destructuring Assignment to Assign Variables from Objects.js
+++ ES6/Use Destructuring Assignment to Assign Variables from Objects.js
@@ -2,11 +2,10 @@ const HIGH_TEMPERATURES = {
     yesterday: 75,
     today: 77,
     tomorrow: 80
-  };
-  
-  // Only change code below this line
-    
-  const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES;
-   
-  
-  // Only change code above this line
+};
+
+//atribuindo variáveis ​​de objetos
+
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+
+
