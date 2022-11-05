a41b03bab513e5353e7d7cbe5921f5d4c1e887c0
Renessa Cerqueira <renessasantos123@gmail.com>
2022-11-05 09:29:33 -0300

    Use a atribuição de desestruturação para extrair valores de objetos

--- ES6/use-destructuring-assignment-to-extract-values-from-objects.js
+++ ES6/use-destructuring-assignment-to-extract-values-from-objects.js
@@ -2,5 +2,6 @@ const HIGH_TEMPERATURES = { yesterday: 75, today: 77,tomorrow: 80
 };
 
 //atribuição de desestruturação 
+//extraindo valores de objetos
 const { today, tomorrow } = HIGH_TEMPERATURES;
-
+console.log(HIGH_TEMPERATURES);
