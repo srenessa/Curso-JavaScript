a472baa7ffe29d713f662dbd3914b1e2fe90cfe4
Renessa Cerqueira <renessasantos123@gmail.com>
2022-11-12 23:36:32 -0300

    Criar strings usando literais de modelo

--- ES6/create-strings-using-template-literals.js
+++ ES6/create-strings-using-template-literals.js
@@ -2,16 +2,16 @@ const result = {
     success: ["max-length", "no-amd", "prefer-arrow-functions"],
     failure: ["no-var", "var-on-top", "linebreak"],
     skipped: ["no-extra-semi", "no-dup-keys"]
-  };
-  function makeList(arr) {
+};
+function makeList(arr) {
     "use strict";
     // change code below this line
     const failureItems = [];
     for (let i = 0; i < arr.length; i++) {
-      failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+        failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
     }
     // change code above this line
     return failureItems;
-  }
-  
-  const failuresList = makeList(result.failure);
+}
+
+const failuresList = makeList(result.failure);
