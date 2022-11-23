55d4eeba37c7ea6a60c9b3e06965c93f94cad317
Renessa Cerqueira <renessasantos123@gmail.com>
2022-11-23 15:42:48 -0300

    Use a sintaxe de classe para definir uma função de construtor

--- ES6/use-class-syntax-to-define-a-constructor-function.js
+++ ES6/use-class-syntax-to-define-a-constructor-function.js
@@ -8,10 +8,3 @@ const carrot = new Vegetable("carrot");
 console.log(carrot.name); // => should be 'carrot'
 
 
-const a = "fulano";
-const b = 'de tal';
-
-const c = a + " " + b;
-console.log(c);
-
-console.log(`${a} ${b}`);
