efaf75beb8e943c41e581aa018100953ecfc913b
Renessa Cerqueira <renessasantos123@gmail.com>
2022-11-30 15:24:32 -0300

    Use * para importar tudo de um arquivo

--- ES6/use-to-import-everything-from-a-file.js
+++ ES6/use-to-import-everything-from-a-file.js
@@ -1,5 +1,4 @@
 import * as stringFunctions from "./string_functions.js";
-// Only change code above this line
 
 stringFunctions.uppercaseString("hello");
 stringFunctions.lowercaseString("WORLD!");
