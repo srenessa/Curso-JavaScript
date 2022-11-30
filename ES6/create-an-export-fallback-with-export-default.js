0b627e6504089932c69ecac269219218fb2d0f32
Renessa Cerqueira <renessasantos123@gmail.com>
2022-11-30 16:19:56 -0300

    Criar um fallback de exportação com padrão de exportação

--- ES6/create-an-export-fallback-with-export-default.js
+++ ES6/create-an-export-fallback-with-export-default.js
@@ -1,4 +1,5 @@
 // você usará essa sintaxe se apenas um valor estiver sendo exportado de um arquivo.
+// não pode usar o padrão de exportação com var, let ou const
 export default function subtract(x, y) {
     return x - y;
 }
