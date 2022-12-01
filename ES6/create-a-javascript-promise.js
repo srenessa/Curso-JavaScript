7b4df34e970433a05e1652b1ea27595aa4edba30
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-01 10:53:51 -0300

    Criar uma promessa de JavaScript

---
+++ ES6/create-a-javascript-promise.js
@@ -0,0 +1,7 @@
+// Criar promessa em JavaScript
+// Promise é uma função construtora, então você precisa usar a palavra-chave new para criar uma.
+// Leva uma função, como argumento, com dois parâmetros - resolver e rejeitar
+
+const makeServerRequest = new Promise((resolve, reject)=>{
+
+});
