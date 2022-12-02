6aec13e24108b7e48163d78252b1423a69556521
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-02 11:04:55 -0300

    Ignorar maiúsculas e minúsculas durante a correspondência

--- regular-expressions/ignore-case-while-matching.js
+++ regular-expressions/ignore-case-while-matching.js
@@ -1,3 +1,5 @@
+//Ignorar maiúsculas e minúsculas durante a correspondência
+//Você pode combinar ambos os casos usando o que é chamado de sinalizador(nesse caso utilizaremos o sinalizador i)  que ignora maiúsculas e minúsculas.
 let myString = "freeCodeCamp";
-let fccRegex = /freeCodeCamp/i; // Change this line
+let fccRegex = /freeCodeCamp/i; 
 let result = fccRegex.test(myString);
