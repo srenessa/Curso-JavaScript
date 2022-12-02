924c58672cb6e4e272420c80425957228d7c9746
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-01 21:25:59 -0300

    Lidar com uma promessa cumprida com então

--- ES6/handle-a-fulfilled-promise-with-then.js
+++ ES6/handle-a-fulfilled-promise-with-then.js
@@ -1,14 +1,14 @@
 const makeServerRequest = new Promise((resolve, reject) => {
 
     let responseFromServer = true;
-      
-    if(responseFromServer) {
-      resolve("We got the data");
-    } else {  
-      reject("Data not received");
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
     }
-  });
-  
-  makeServerRequest.then(result =>{
-  console.log(result);
-  });
+});
+
+makeServerRequest.then(result => {
+    console.log(result);
+});
