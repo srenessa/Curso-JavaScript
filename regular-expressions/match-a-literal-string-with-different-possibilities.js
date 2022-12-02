68b73b5fbb67b47f4f992ade78c4964c4e88df17
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-02 10:50:15 -0300

    Combine uma string literal com diferentes possibilidades

--- regular-expressions/match-a-literal-string-with-different-possibilities.js
+++ regular-expressions/match-a-literal-string-with-different-possibilities.js
@@ -1,3 +1,5 @@
 let petString = "James has a pet cat.";
-let petRegex = /dog|cat|bird|fish/; // Change this line
+//Você pode pesquisar vários padrões usando a alternância ou o operador OR(OU): |
+//Você também pode pesquisar mais do que apenas dois padrões. Você pode fazer isso adicionando mais padrões com mais operadores OR separando-os, como /yes|no|talvez/.
+let petRegex = /dog|cat|bird|fish/; 
 let result = petRegex.test(petString);
