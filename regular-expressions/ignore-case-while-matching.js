05a5fd54d21104c4b54876b10c1bbb701f107777
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-02 19:21:23 -0300

    Extrair Correspondências

--- regular-expressions/ignore-case-while-matching.js
+++ regular-expressions/ignore-case-while-matching.js
@@ -1,5 +1,5 @@
 //Ignorar maiúsculas e minúsculas durante a correspondência
 //Você pode combinar ambos os casos usando o que é chamado de sinalizador(nesse caso utilizaremos o sinalizador i)  que ignora maiúsculas e minúsculas.
 let myString = "freeCodeCamp";
-let fccRegex = /freeCodeCamp/i; 
+let fccRegex = /FreeCodeCamp/i; 
 let result = fccRegex.test(myString);
