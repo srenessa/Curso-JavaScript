4b8adef0b67cb27c8f69979ead7c14064dd04c9e
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-01 21:59:50 -0300

    Lidar com uma promessa rejeitada com catch

--- ES6/handle-a-rejected-promise-with-catch.js
+++ ES6/handle-a-rejected-promise-with-catch.js
@@ -1,14 +1,15 @@
 const makeServerRequest = new Promise((resolve, reject) => {
-    // responseFromServer is set to false to represent an unsuccessful response from a server
+
     let responseFromServer = false;
-      
-    if(responseFromServer) {
-      resolve("We got the data");
-    } else {  
-      reject("Data not received");
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
     }
-  });
-  
-  makeServerRequest.then(result => {
-    console.log(result);
-  });
+});
+
+  //catch é o método usado quando sua promessa foi rejeitada
+makeServerRequest.catch(error => {
+    console.log(error);
+});
