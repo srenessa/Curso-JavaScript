8fc8d0cd35c13cbdaca788e50ff315848ea842d5
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-02 09:37:29 -0300

    Usando o método de teste

--- regular-expressions/using-the-test-method.js
+++ regular-expressions/using-the-test-method.js
@@ -1,3 +1,6 @@
+//Uma maneira de testar um regex é usar o método .test()
+//as aspas não são necessárias na expressão regular.
+//O método .test() pega o regex, aplica-o a uma string (que é colocada entre parênteses) e retorna verdadeiro ou falso se seu padrão encontrar algo ou não.
 let myString = "Hello, World!";
 let myRegex = /Hello/;
-let result = myRegex.test(myString); // Change this line
+let result = myRegex.test(myString); 
