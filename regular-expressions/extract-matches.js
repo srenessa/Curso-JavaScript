69d4407d08107aeef5c7360c50fc905022970d11
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-05 19:44:19 -0300

    Extrair Correspondências

--- regular-expressions/extract-matches.js
+++ regular-expressions/extract-matches.js
@@ -1,7 +1,8 @@
 //Extraindo correspondências reais encontradas com o método .match().
 //Para usar o método .match(), aplique o método em uma string e passe o regex dentro dos parênteses.
 //A sintaxe .match é o "oposto" do método .test 
-
+// .test /regex/.test('string'); 
+// .match = 'string'.match(/regex/);
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/;
 let result = extractStr.match(codingRegex); 
