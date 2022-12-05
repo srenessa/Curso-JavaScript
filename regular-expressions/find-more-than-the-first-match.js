8c07b515aee748df7c9c30ebec2a52003c4172e3
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-05 19:25:53 -0300

    Encontre mais do que a primeira correspondência

--- regular-expressions/find-more-than-the-first-match.js
+++ regular-expressions/find-more-than-the-first-match.js
@@ -1,3 +1,5 @@
+//Para pesquisar ou extrair um padrão mais de uma vez, você pode usar o sinalizador de pesquisa global: g.
+// Utilizaremos o sinalizador: i para ignorar maiúsculas e minúsculas.
 let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /Twinkle/gi; 
 let result = twinkleStar.match(starRegex); 
