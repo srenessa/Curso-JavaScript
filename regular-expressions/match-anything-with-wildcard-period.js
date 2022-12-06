b8129d6d8720b3c3706d10bd0cf815ae25a68762
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-06 16:08:30 -0300

    Combine qualquer coisa com o período curinga

--- regular-expressions/match-anything-with-wildcard-period.js
+++ regular-expressions/match-anything-with-wildcard-period.js
@@ -1,3 +1,7 @@
+// caractere curinga: . / O curinga também é chamado de ponto
+// Você pode usar o caractere curinga como qualquer outro caractere no regex. 
+// Complete o regex unRegex para que corresponda às strings run, sun, fun, pun, nun e bun
+// Dica: procurar um ponto em comum entre strings, nesse caso seria o "un"  e utilizamos o caractere curinga: . 
 
 let exampleStr = "Let's have fun with regular expressions!";
 let unRegex = /.un/; 
