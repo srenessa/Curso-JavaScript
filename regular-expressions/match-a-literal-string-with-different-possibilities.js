1d998899769c20738b469a310e19929b08c89782
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-07 19:00:40 -0300

    Combinar Letras do Alfabeto

--- regular-expressions/match-a-literal-string-with-different-possibilities.js
+++ regular-expressions/match-a-literal-string-with-different-possibilities.js
@@ -1,5 +1,5 @@
 let petString = "James has a pet cat.";
 //Você pode pesquisar vários padrões usando a alternância ou o operador OR(OU): |
-//Você também pode pesquisar mais do que apenas dois padrões. Você pode fazer isso adicionando mais padrões com mais operadores OR separando-os, como /yes|no|talvez/.
+//Você também pode pesquisar mais do que apenas dois padrões. Você pode fazer isso adicionando mais padrões com mais operadores OR separando-os, como /yes|no|talvez/
 let petRegex = /dog|cat|bird|fish/; 
 let result = petRegex.test(petString);
--- regular-expressions/match-letters-of-the-alphabet.js
+++ regular-expressions/match-letters-of-the-alphabet.js
@@ -1,8 +1,12 @@
 // Combinar Letras do Alfabeto
 // Você pode definir um intervalo de caracteres para correspondência usando um hífen: -
 // Para corresponder às letras minúsculas de a a e, você usaria [a-e]
+// Para pesquisar ou extrair um padrão mais de uma vez, você pode usar o sinalizador de pesquisa global: g.
+// Utilizaremos o sinalizador: i para ignorar maiúsculas e minúsculas.
 
 let quoteSample = "The quick brown fox jumps over the lazy dog.";
 let alphabetRegex = /[a-z]/gi; 
 let result = quoteSample.match(alphabetRegex);
+
+console.log(result);
 
