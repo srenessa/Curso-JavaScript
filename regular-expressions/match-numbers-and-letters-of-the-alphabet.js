0af151071e954156a538259ddef463c7582785bb
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-07 19:29:53 -0300

    Combinar Números e Letras do Alfabeto

--- regular-expressions/match-numbers-and-letters-of-the-alphabet.js
+++ regular-expressions/match-numbers-and-letters-of-the-alphabet.js
@@ -1,3 +1,8 @@
+//O hífen (-) também funciona para corresponder a um intervalo de números
+//[0-5]/ corresponde a qualquer número entre 0 e 5, incluindo 0 e 5.
+//É possível combinar uma variedade de letras e números em um único conjunto de caracteres
+
 let quoteSample = "Blueberry 3.141592653s are delicious.";
 let myRegex = /[h-s2-6]/gi; 
-let result =  quoteSample.match(myRegex); 
+let result =  quoteSample.match(myRegex);
+
