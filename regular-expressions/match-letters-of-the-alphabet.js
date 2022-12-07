c5063c121dbe1f8d1f1b5f820cdf6feb405b7c45
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-07 18:59:04 -0300

    Combinar Letras do Alfabeto

--- regular-expressions/match-letters-of-the-alphabet.js
+++ regular-expressions/match-letters-of-the-alphabet.js
@@ -1,4 +1,8 @@
+// Combinar Letras do Alfabeto
+// Você pode definir um intervalo de caracteres para correspondência usando um hífen: -
+// Para corresponder às letras minúsculas de a a e, você usaria [a-e]
+
 let quoteSample = "The quick brown fox jumps over the lazy dog.";
-let alphabetRegex = /[a-z]/gi; // Change this line
-let result = quoteSample.match(alphabetRegex); // Change this line
+let alphabetRegex = /[a-z]/gi; 
+let result = quoteSample.match(alphabetRegex);
 
