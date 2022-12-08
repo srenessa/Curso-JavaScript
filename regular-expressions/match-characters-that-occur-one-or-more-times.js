46a8eda2b5403daf57a79eb342e63ec053a8f8fe
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-08 15:22:31 -0300

    Corresponde a caracteres que ocorrem uma ou mais vezes

--- regular-expressions/match-characters-that-occur-one-or-more-times.js
+++ regular-expressions/match-characters-that-occur-one-or-more-times.js
@@ -1,3 +1,11 @@
+// Corresponde a caracteres que ocorrem uma ou mais vezes 
+
+// Você pode usar o caractere + para verificar  a um caractere (ou grupo de caracteres) que aparece uma ou mais vezes seguidas
+
+// Por exemplo, /a+/g encontraria uma correspondência em abc e retornaria ["a"]. Por causa do +, ele também encontraria uma única correspondência em aabc e retornaria ["aa"].
+
+// Você deseja encontrar correspondências quando a letra s ocorre uma ou mais vezes no Mississippi
+
 let difficultSpelling = "Mississippi";
 let myRegex = /s+/gi; 
 let result = difficultSpelling.match(myRegex);
