a5b5f5735d96945085f18d57bbadd97908457ceb
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-08 16:44:56 -0300

    Corresponde a caracteres que ocorrem zero ou mais vezes

--- regular-expressions/match-characters-that-occur-zero-or-more-times.js
+++ regular-expressions/match-characters-that-occur-zero-or-more-times.js
@@ -1,3 +1,6 @@
+// corresponde a caracteres que ocorrem zero ou mais vezes
+// O caractere para fazer isso é o asterisco ou estrela: *
+
 let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
 let chewieRegex = /Aa*/; 
 let result = chewieQuote.match(chewieRegex);
