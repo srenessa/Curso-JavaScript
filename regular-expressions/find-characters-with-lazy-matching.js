36b6d4ca8baf7f6168b96d69ac537c8b83100d4b
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-09 15:28:36 -0300

    Encontrar caracteres com correspondência preguiçosa

--- regular-expressions/find-characters-with-lazy-matching.js
+++ regular-expressions/find-characters-with-lazy-matching.js
@@ -1,8 +1,10 @@
 // Encontrar caracteres com correspondência preguiçosa 
 
-// Você pode aplicar o regex /t[a-z]*i/ à string "titanic". Este regex é basicamente um padrão que começa com t, termina com i e tem algumas letras no meio.
+// Você pode aplicar o regex /t[a-z]*i/ à string "titanic". Este regex é basicamente um padrão que começa com t, termina com i e tem algumas letras no meio
 
-// você pode usar o ? caractere para alterá-lo para correspondência preguiçosa.
+// você pode usar o ? caractere para alterá-lo para correspondência preguiçosa
+
+// Corrija o regex /<.*>/ para retornar a tag HTML <h1> e não o texto "<h1>
 
 let text = "<h1>Winter is coming</h1>";
 let myRegex = /<.*?>/; 
