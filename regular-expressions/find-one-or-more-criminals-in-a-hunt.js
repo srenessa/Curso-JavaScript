380bad6b75aa12a46478b089c1dc20ee346675fa
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-19 18:50:30 -0300

    Encontre um ou mais criminosos em uma caçada

--- regular-expressions/find-one-or-more-criminals-in-a-hunt.js
+++ regular-expressions/find-one-or-more-criminals-in-a-hunt.js
@@ -1,1 +1,8 @@
+// A regex /z+/ corresponde à letra z quando ela aparece uma ou mais vezes seguidas.
+
+// Escreva um regex ganancioso que encontre um ou mais criminosos dentro de um grupo de outras pessoas.
+
+// Um criminoso é representado pela letra maiúscula C.
+
 let reCriminals = /C+/;
+
