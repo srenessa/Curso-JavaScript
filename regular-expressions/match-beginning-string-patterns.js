e1c4ed3daf4d0be81f8915a55ceddab5c23d8bd2
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-20 12:06:05 -0300

    Correspondência de padrões iniciais de string

--- regular-expressions/match-beginning-string-patterns.js
+++ regular-expressions/match-beginning-string-patterns.js
@@ -1,3 +1,7 @@
+// Correspondência de padrões iniciais de string
+
+// Além de usar o o caractere circunflexo (^) dentro de um conjunto de caracteres para criar um conjunto de caracteres negadoa, o ircunflexo (^) é usado também para procurar padrões no início de strings.
+
 let rickyAndCal = "Cal and Ricky both like racing.";
 let calRegex = /^Cal/; 
 let result = calRegex.test(rickyAndCal);
