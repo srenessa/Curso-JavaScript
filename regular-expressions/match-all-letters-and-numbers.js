8efd94a3e9906ec3aad23a3afc4679bf5968d852
Renessa Cerqueira <renessasantos123@gmail.com>
2022-12-28 12:46:28 -0300

    Combine todas as letras e números

--- regular-expressions/match-all-letters-and-numbers.js
+++ regular-expressions/match-all-letters-and-numbers.js
@@ -1,3 +1,5 @@
+// Combine todas as letras e números
+
 let quoteSample = "The five boxing wizards jump quickly.";
 let alphabetRegexV2 = /change/; 
 let result = quoteSample.match(alphabetRegexV2).length;
