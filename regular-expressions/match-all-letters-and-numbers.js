6f7300b5b69d41f3b99ae58079b8d43d84beba28
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-04 18:24:44 -0300

    Combine todas as letras e números

--- regular-expressions/match-all-letters-and-numbers.js
+++ regular-expressions/match-all-letters-and-numbers.js
@@ -2,7 +2,10 @@
 
 // A classe de caracteres mais próxima em JavaScript para corresponder ao alfabeto é \w. 
 
-// // Este atalho é igual a [A-Za-z0-9_] ou seja, esta classe de caracteres corresponde a letras maiúsculas e minúsculas e números. 
+// Este atalho é igual a [A-Za-z0-9_] ou seja, esta classe de caracteres corresponde a letras maiúsculas e minúsculas e números. 
+
+// Essa classe de caracteres também inclui o caractere de sublinhado (_).
+
 let quoteSample = "The five boxing wizards jump quickly.";
 let alphabetRegexV2 =  /\w/g; 
 let result = quoteSample.match(alphabetRegexV2).length;
