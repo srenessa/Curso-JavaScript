dcbefbfcbc365f62711ddc7749489020c3a73638
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-09 18:22:05 -0300

    Combine tudo menos letras e números

--- regular-expressions/match-everything-but-letters-and-numbers.js
+++ regular-expressions/match-everything-but-letters-and-numbers.js
@@ -1,3 +1,7 @@
+// Você pode procurar o oposto do \w com \W.
+
+// // Use a classe de caractere abreviada \W para contar o número de caracteres não alfanuméricos em várias aspas e strings
+
 let quoteSample = "The five boxing wizards jump quickly.";
 let nonAlphabetRegex = /\W/g; // Change this line
 let result = quoteSample.match(nonAlphabetRegex).length;
