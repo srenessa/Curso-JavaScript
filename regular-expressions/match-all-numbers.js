1bd95b3945903eba207d5b6d24c520a49144f451
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-10 16:39:40 -0300

    Corresponder a todos os números

--- regular-expressions/match-all-numbers.js
+++ regular-expressions/match-all-numbers.js
@@ -1,3 +1,9 @@
+// Outro padrão comum é procurar apenas dígitos ou números.
+
+// O atalho para procurar caracteres de dígitos é \d, com um d minúsculo.
+
+// Isso é igual à classe de caracteres [0-9], que procura um único caractere de qualquer número entre zero e nove.
+
 let movieName = "2001: A Space Odyssey";
 let numRegex = /\d/g; // Change this line
 let result = movieName.match(numRegex).length;
