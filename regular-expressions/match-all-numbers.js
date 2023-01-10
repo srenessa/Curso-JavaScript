336596b8f696af1e2affde01946aefc7a1ab34fd
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-10 16:40:56 -0300

    Corresponder a todos os números

--- regular-expressions/match-all-numbers.js
+++ regular-expressions/match-all-numbers.js
@@ -5,5 +5,5 @@
 // Isso é igual à classe de caracteres [0-9], que procura um único caractere de qualquer número entre zero e nove.
 
 let movieName = "2001: A Space Odyssey";
-let numRegex = /\d/g; // Change this line
+let numRegex = /\d/g; 
 let result = movieName.match(numRegex).length;
