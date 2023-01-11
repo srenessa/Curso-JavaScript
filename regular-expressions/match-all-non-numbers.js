073e8981a32207ab1577e64b91e6c87944847bd2
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-11 18:21:00 -0300

    Corresponder a todos os não-números

--- regular-expressions/match-all-non-numbers.js
+++ regular-expressions/match-all-non-numbers.js
@@ -1,3 +1,7 @@
+// Corresponder a todos os não-números
+
+// O atalho para procurar caracteres que não sejam dígitos é \D 
+
 let movieName = "2001: A Space Odyssey";
 let noNumRegex = /\D/g; 
 let result = movieName.match(noNumRegex).length;
