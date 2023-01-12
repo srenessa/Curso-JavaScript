4b6cb4020dce8bbd1cab632ad6eb068f68ba3b67
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-12 17:47:50 -0300

    Restringir possíveis nomes de usuário

--- regular-expressions/restrict-possible-usernames.js
+++ regular-expressions/restrict-possible-usernames.js
@@ -1,3 +1,3 @@
 let username = "JackOfAllTrades";
-let userCheck = /change/; // Change this line
+let userCheck =  /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i; // Change this line
 let result = userCheck.test(username);
