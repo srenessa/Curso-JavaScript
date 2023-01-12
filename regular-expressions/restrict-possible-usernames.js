ad2913fdab6c099a501f6b3145c69df51e0e28f1
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-12 18:40:16 -0300

    Restringir possíveis nomes de usuário

--- regular-expressions/restrict-possible-usernames.js
+++ regular-expressions/restrict-possible-usernames.js
@@ -12,14 +12,12 @@ let username = "JackOfAllTrades";
 let userCheck =  /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i; 
 let result = userCheck.test(username);
 
-// O circunflexo (^) é usado também para procurar padrões no início de strings
-
- // Você pode definir um intervalo de caracteres para correspondência usando um hífen: -, ou seja de a a z
-
- // O atalho para procurar caracteres de dígitos é \d, com um d minúsculo.
-
-// Você pode pesquisar o final das strings usando o caractere cifrão $ no final do regex.
-
-// Utilizaremos o sinalizador: i para ignorar maiúsculas e minúsculas.
-
+// 1 - ^ - início da entrada
+// 2 - [a-z] - o primeiro caractere é uma letra
+// 3 - [a-z]+ - os seguintes caracteres são letras
+// 4 - \d*$ - a entrada termina com 0 ou mais dígitos
+// 5 - | - ou
+// 6 - ^[a-z] - primeiro caractere é uma letra
+// 7 - \d\d+ - os seguintes caracteres são 2 ou mais dígitos
+// 8 - $ - fim da entrada
 
