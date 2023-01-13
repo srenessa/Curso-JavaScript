d83999dd9f81b6625978e5b7a0765da1f3368003
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-13 17:35:03 -0300

    Especifique apenas o menor número de correspondências

--- regular-expressions/specify-only-the-lower-number-of-matches.js
+++ regular-expressions/specify-only-the-lower-number-of-matches.js
@@ -1,3 +1,9 @@
+// Às vezes, você deseja especificar apenas o número inferior de padrões sem limite superior.
+
+// Para especificar apenas o menor número de padrões, mantenha o primeiro número seguido por uma vírgula.
+
+// Altere o regex haRegex para corresponder à palavra Hazzah somente quando tiver quatro ou mais letras z.
+
 let haStr = "Hazzzzah";
 let haRegex = /Haz{4,}ah/; 
 let result = haRegex.test(haStr);
