c7de60ec8cb129be46d6c43a4f4edf2d7bb77573
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-13 15:40:08 -0300

    Corresponder caracteres sem espaço em branco

--- regular-expressions/match-non-whitespace-characters.js
+++ regular-expressions/match-non-whitespace-characters.js
@@ -1,3 +1,3 @@
 let sample = "Whitespace is important in separating words";
-let countNonWhiteSpace = /change/; // Change this line
+let countNonWhiteSpace = /\S/g; 
 let result = sample.match(countNonWhiteSpace);
