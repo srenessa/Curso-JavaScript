61e60b10b7903db2805af9c47a63f7a6dc9f65d5
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-13 16:36:06 -0300

    Corresponder caracteres sem espaço em branco

--- regular-expressions/match-non-whitespace-characters.js
+++ regular-expressions/match-non-whitespace-characters.js
@@ -1,3 +1,9 @@
+// Pesquise por não-espaço em branco.
+
+// Pesquisar tudo, exceto espaços em branco usando \S, que é um s maiúsculo.
+
+// Semelhante à classe de caracteres [^ \r\t\f\n\v].
+
 let sample = "Whitespace is important in separating words";
 let countNonWhiteSpace = /\S/g; 
 let result = sample.match(countNonWhiteSpace);
