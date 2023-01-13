e1ceaafed05d5ce1b5031cde072a636a7485cbd9
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-13 17:26:35 -0300

    Especifique o número superior e inferior de correspondências

--- regular-expressions/specify-upper-and-lower-number-of-matches.js
+++ regular-expressions/specify-upper-and-lower-number-of-matches.js
@@ -1,3 +1,11 @@
+// Especifique o número superior e inferior de correspondências.
+
+// Usa-se o sinal de mais + para procurar um ou mais caracteres e o asterisco * para procurar zero ou mais caracteres.
+
+// Você pode especificar o número inferior e superior de padrões com especificadores de quantidade.
+
+// Especificadores de quantidade são usados ​​com chaves ({ e }). Você coloca dois números entre as chaves - para o número inferior e superior de padrões.
+
 let ohStr = "Ohhh no";
 let ohRegex = /Oh{3,6}\sno/; 
 let result = ohRegex.test(ohStr);
