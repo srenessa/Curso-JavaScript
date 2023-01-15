e5665b2c5ad6f58fdaa280a26443c8f9f1eb6bd6
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-15 18:29:36 -0300

    Verifique se há todos ou nenhum

--- regular-expressions/check-for-all-or-none.js
+++ regular-expressions/check-for-all-or-none.js
@@ -1,3 +1,10 @@
+// Verifique se há todos ou nenhum.
+
+// // Às vezes, os padrões que você deseja pesquisar podem ter partes que podem ou não existir. No entanto, pode ser importante verificá-los
+
+// Você pode especificar a possível existência de um elemento com um ponto de interrogação, ?.|
+
 let favWord = "favorite";
 let favRegex = /favou?rite/; 
 let result = favRegex.test(favWord);
+
