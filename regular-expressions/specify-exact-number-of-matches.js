3b4154b0ec53e34da58675d0d32e77783c52fa4e
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-15 17:41:03 -0300

    Especifique o número exato de correspondências

--- regular-expressions/specify-exact-number-of-matches.js
+++ regular-expressions/specify-exact-number-of-matches.js
@@ -1,3 +1,9 @@
+// Especifique o número exato de correspondências, ou seja, apenas um número específico de correspondências.
+
+// Para especificar um certo número de padrões, basta colocar esse número entre as chaves.
+
+// Altere o regex timRegex para corresponder à palavra Timber somente quando ela tiver quatro letras m.
+
 let timStr = "Timmmmber";
 let timRegex = /Tim{4}ber/; 
 let result = timRegex.test(timStr);
