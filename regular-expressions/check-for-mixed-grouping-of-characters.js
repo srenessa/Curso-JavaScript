0cd52fc0e6b7e2e0273014fc4d03deccfdb94faa
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-17 14:10:17 -0300

    Verifique se há agrupamento misto de caracteres

--- regular-expressions/check-for-mixed-grouping-of-characters.js
+++ regular-expressions/check-for-mixed-grouping-of-characters.js
@@ -1,4 +1,3 @@
 let myString = "Eleanor Roosevelt";
-let myRegex = /False/; // Change this line
-let result = false; // Change this line
-// After passing the challenge experiment with myString and see how the grouping works
+let myRegex = /False/; 
+let result = false; 
