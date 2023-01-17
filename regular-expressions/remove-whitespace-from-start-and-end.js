5b0a9629275a551e720d7e8c1648527adde463db
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-17 18:00:35 -0300

    Remova o espaço em branco do início e do fim

--- regular-expressions/remove-whitespace-from-start-and-end.js
+++ regular-expressions/remove-whitespace-from-start-and-end.js
@@ -1,3 +1,7 @@
+//  Remover o espaço em branco no início e no final da string.
+
+// Escreva um regex e use os métodos de string apropriados para remover os espaços em branco no início e no final das strings, usando  expressões regulares.
+
 let hello = "   Hello, World!  ";
 let wsRegex =  /^\s+|\s+$/g; 
 let result = hello.replace(wsRegex, "");
