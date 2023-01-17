528888a04b79abae62c3f61ba7bcb66efbadfc32
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-17 14:28:04 -0300

    Verifique se há agrupamento misto de caracteres

--- regular-expressions/check-for-mixed-grouping-of-characters.js
+++ regular-expressions/check-for-mixed-grouping-of-characters.js
@@ -4,7 +4,8 @@
 
 // Em seguida, verifique se os grupos de strings desejados estão na string de teste usando o método test().
 
-// Corrija o regex para que ele verifique os nomes de Franklin Roosevelt ou Eleanor Roosevelt de maneira sensível a maiúsculas e minúsculas e deve fazer concessões para nomes do meio
+// Corrija o regex para que ele verifique os nomes de Franklin Roosevelt ou Eleanor Roosevelt de maneira sensível a maiúsculas e minúsculas e deve fazer concessões para nomes do meio.
+
 let myString = "Eleanor Roosevelt";
 let myRegex = /(Franklin|Eleanor) (([A-Z]\.?|[A-Z][a-z]+) )?Roosevelt/; 
 let result = myRegex.test(myString); 
