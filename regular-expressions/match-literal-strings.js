9f81dddedcdf9081c5155a1205495069e6f18b60
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-18 16:24:26 -0300

    Match Literal Strings

--- regular-expressions/match-literal-strings.js
+++ regular-expressions/match-literal-strings.js
@@ -1,4 +1,5 @@
-let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 //Qualquer outra forma de Waldo não corresponderá. Por exemplo, o regex /waldo/(com o w minusculo) não corresponderá, nem WALDO(todo maiusculo).
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/; 
 let result = waldoRegex.test(waldoIsHiding);
