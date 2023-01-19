421854119314539af13776a2def488ac792d39dd
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-19 18:48:08 -0300

    Combine um único personagem com múltiplas possibilidades

--- regular-expressions/match-single-character-with-multiple-possibilities.js
+++ regular-expressions/match-single-character-with-multiple-possibilities.js
@@ -1,5 +1,7 @@
 // Combine um único personagem com múltiplas possibilidades.
 
+// É o equilíbrio entre os dois extremos: padrões literais (/literal/) e caractere curinga (/./).
+
 // Use uma classe de caracteres com vogais (a, e, i, o, u) em seu vogalRegex regex para encontrar todas as vogais na string quoteSample.
 
 // Por exemplo, você deseja combinar bag, big e bug. Você pode criar o regex /b[aiu]g/ para fazer isso. O [aiu] é a classe de caracteres que corresponderá apenas aos caracteres a, i ou u.
