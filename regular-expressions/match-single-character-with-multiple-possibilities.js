bc58fe7ebdb697c065f8523cb615eefe0835f42b
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-19 11:55:25 -0300

    Combine um único personagem com múltiplas possibilidades

--- regular-expressions/match-single-character-with-multiple-possibilities.js
+++ regular-expressions/match-single-character-with-multiple-possibilities.js
@@ -3,6 +3,7 @@
 // Por exemplo, você deseja combinar bag, big e bug. Você pode criar o regex /b[aiu]g/ para fazer isso. O [aiu] é a classe de caracteres que corresponderá apenas aos caracteres a, i ou u.
 
 // Para pesquisar ou extrair um padrão mais de uma vez, você pode usar o sinalizador de pesquisa global: g.
+
 // Utilizaremos o sinalizador: i para ignorar maiúsculas e minúsculas.
 
 let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
