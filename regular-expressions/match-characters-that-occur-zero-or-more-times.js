d92a9ad74be75dbca548b782dea977ec46ceba3e
Renessa Cerqueira <renessasantos123@gmail.com>
2023-01-23 23:04:26 -0300

    Corresponde a caracteres que ocorrem zero ou mais vezes

--- regular-expressions/match-characters-that-occur-zero-or-more-times.js
+++ regular-expressions/match-characters-that-occur-zero-or-more-times.js
@@ -1,5 +1,7 @@
 // Corresponde a caracteres que ocorrem zero ou mais vezes
+
 // O caractere para fazer isso é o asterisco ou estrela: *
+
 //  Em /go*/ - "gooooooooal!"; , "gut feeling"; ,  "over the moon"; - Em ordem, as três chamadas de correspondência retornariam os valores ["goooooooo"], ["g"] e nulo.
 
 let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
