92db586d54ec08eaf1ee7497d750a63c61b9fa06
Renessa Cerqueira <renessasantos123@gmail.com>
2023-02-06 16:32:45 -0300

    Corresponder a todos os não-números

--- regular-expressions/match-all-non-numbers.js
+++ regular-expressions/match-all-non-numbers.js
@@ -2,6 +2,8 @@
 
 // O atalho para procurar caracteres que não sejam dígitos é \D. Isso é igual à classe de caracteres [^0-9], que procura um único caractere que não seja um número entre zero e nove.
 
+// Nesse caso retornaria o numero 17, por ter 17 letras e caracteres.
+
 let movieName = "2001: A Space Odyssey";
 let noNumRegex = /\D/g; 
 let result = movieName.match(noNumRegex).length;
