45fc73ea8747084715c8567952efe6e2a6cc23f9
Renessa Cerqueira <renessasantos123@gmail.com>
2023-02-06 15:43:14 -0300

    Correspondência de padrões de string de finalização

--- regular-expressions/match-ending-string-patterns.js
+++ regular-expressions/match-ending-string-patterns.js
@@ -2,6 +2,8 @@
 
 // Você pode pesquisar o final das strings usando o caractere cifrão $ no final do regex.
 
+// Nesse caso o teste daria true, porque a ultima palavra da frase é caboose, passada no parametro de busca.
+
 let caboose = "The last car on a train is the caboose";
 let lastRegex = /caboose$/;
 let result = lastRegex.test(caboose);
