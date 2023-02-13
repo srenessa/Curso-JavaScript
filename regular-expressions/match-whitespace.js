fa623b986b64f73b97af05a46ea054b5137f6bd6
Renessa Cerqueira <renessasantos123@gmail.com>
2023-02-13 10:28:33 -0300

    Corresponder ao espaço em branco

--- regular-expressions/match-whitespace.js
+++ regular-expressions/match-whitespace.js
@@ -1,9 +1,12 @@
 // Você pode procurar espaços em branco usando \s, que é um s minúsculo.
 
-// Esse padrão não apenas corresponde a espaços em branco, mas também a caracteres de retorno de carro, tabulação, alimentação de formulário e nova linha
+// Esse padrão não apenas corresponde a espaços em branco, mas também a caracteres de retorno de carro, tabulação, alimentação de formulário e nova linha.
+
 
 let sample = "Whitespace is important in separating words";
 let countWhiteSpace = /\s/g; 
 let result = sample.match(countWhiteSpace);
+
+// Esta chamada de correspondência retornaria [" ", " "].
 
 console.log(result);
