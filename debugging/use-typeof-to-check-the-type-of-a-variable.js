3c5a9074555b5ade200cb65fd3b1b2a17738dfe1
Renessa Cerqueira <renessasantos123@gmail.com>
2023-03-21 18:22:41 -0300

    Use typeof para verificar o tipo de uma variável

--- debugging/use-typeof-to-check-the-type-of-a-variable.js
+++ debugging/use-typeof-to-check-the-type-of-a-variable.js
@@ -1,3 +1,11 @@
+// Use typeof para verificar o tipo de uma variável
+
+// Você pode usar typeof para verificar a estrutura de dados, ou tipo, de uma variável.
+
+// O JavaScript reconhece sete tipos de dados primitivos (imutáveis): Boolean, Null, Undefined, Number, String, Symbol (novo com ES6) e BigInt (novo com ES2020) e um tipo para itens mutáveis: Object.
+
+// Adicione duas instruções console.log() para verificar o tipo de cada uma das duas variáveis ​​sete e três no código.
+
 let seven = 7;
 let three = "3";
 console.log(seven + three);
