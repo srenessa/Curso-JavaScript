dfca8f500757096ccfca09c5da7190b5969930e3
Renessa Cerqueira <renessasantos123@gmail.com>
2023-03-21 10:53:19 -0300

    Use o console JavaScript para verificar o valor de uma variável

--- debugging/use-the-javascript-console-to-check-the-value-of-a-variable.js
+++ debugging/use-the-javascript-console-to-check-the-value-of-a-variable.js
@@ -1,5 +1,7 @@
 // Use o console JavaScript para verificar o valor de uma variável
 
+//O método console.log(), que "imprime" a saída do que está entre seus parênteses no console, provavelmente será a ferramenta de depuração mais útil.
+
 let a = 5;
 let b = 1;
 a++;
