deb82dc787bbe8d5ff69b4dd5a23186f0a7143ae
Renessa Cerqueira <renessasantos123@gmail.com>
2023-03-21 16:19:50 -0300

    Compreendendo as diferenças entre o freeCodeCamp e o console do navegador

--- debugging/understanding-the-differences-between-the-freecodecamp-and-browser-console.js
+++ debugging/understanding-the-differences-between-the-freecodecamp-and-browser-console.js
@@ -1,3 +1,13 @@
+// Compreendendo as diferenças entre o freeCodeCamp e o console do navegador
+
+// Existem muitos métodos para usar com o console para enviar mensagens. log, warning e clear, por exemplo.
+
+// O console do freeCodeCamp produzirá apenas mensagens de log, enquanto o console do navegador exibirá todas as mensagens.
+
+// Use console.log para registrar a variável de saída.
+
+// Finalmente, use console.clear após seu log para limpar o console do navegador. Veja a diferença nos dois consoles.
+
 let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
 
 console.log(output);
