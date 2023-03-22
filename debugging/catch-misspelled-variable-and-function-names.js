7cd31dbb4a6457f206fd2a440ee783dea6d04c9d
Renessa Cerqueira <renessasantos123@gmail.com>
2023-03-22 11:16:44 -0300

    Capturar nomes de variáveis ​​e funções com erros ortográficos

---
+++ debugging/catch-misspelled-variable-and-function-names.js
@@ -0,0 +1,16 @@
+
+/* antes
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = recievables - payable;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+ */
+
+
+
+//depois
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+
