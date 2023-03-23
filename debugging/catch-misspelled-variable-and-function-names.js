3253dad3d46d36a7ac276cff927567d9a2647c3c
Renessa Cerqueira <renessasantos123@gmail.com>
2023-03-23 18:40:12 -0300

    Capturar nomes de variáveis ​​e funções com erros ortográficos

--- debugging/catch-misspelled-variable-and-function-names.js
+++ debugging/catch-misspelled-variable-and-function-names.js
@@ -1,3 +1,5 @@
+// Uma das formas comuns que os bugs assumem é um problema de nível de sintaxe, o erro de ortografia.
+
 
 /* antes
 let receivables = 10;
@@ -5,8 +7,6 @@ let payables = 8;
 let netWorkingCapital = recievables - payable;
 console.log(`Net working capital is: ${netWorkingCapital}`);
  */
-
-
 
 //depois
 let receivables = 10;
