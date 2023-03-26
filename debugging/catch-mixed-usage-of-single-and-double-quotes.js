adcc49d8305e5bee8455b4015ef46fd1e766520d
Renessa Cerqueira <renessasantos123@gmail.com>
2023-03-26 13:06:25 -0300

    Capturar o uso misto de aspas simples e duplas

--- debugging/catch-mixed-usage-of-single-and-double-quotes.js
+++ debugging/catch-mixed-usage-of-single-and-double-quotes.js
@@ -1,4 +1,6 @@
 // Capturar o uso misto de aspas simples e duplas
 
+// JavaScript permite o uso de aspas simples (') e duplas (") para declarar uma string.
+
 let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
 console.log(innerHtml);
