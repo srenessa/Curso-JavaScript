65b1399b7544cfe00cce193411446b17cf60301e
Renessa Cerqueira <renessasantos123@gmail.com>
2023-03-26 13:10:25 -0300

    Capturar o uso misto de aspas simples e duplas

--- debugging/catch-mixed-usage-of-single-and-double-quotes.js
+++ debugging/catch-mixed-usage-of-single-and-double-quotes.js
@@ -2,5 +2,7 @@
 
 // JavaScript permite o uso de aspas simples (') e duplas (") para declarar uma string.
 
+// Não há problema em usar apenas um estilo de aspas. Você pode escapar das aspas dentro da string usando o caractere de escape de barra invertida (\):
+
 let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
 console.log(innerHtml);
