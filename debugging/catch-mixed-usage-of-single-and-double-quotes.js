4f670129af7feb9b4753c6675bd17ade44fcd37e
Renessa Cerqueira <renessasantos123@gmail.com>
2023-03-26 13:11:00 -0300

    Capturar o uso misto de aspas simples e duplas

--- debugging/catch-mixed-usage-of-single-and-double-quotes.js
+++ debugging/catch-mixed-usage-of-single-and-double-quotes.js
@@ -4,5 +4,7 @@
 
 // Não há problema em usar apenas um estilo de aspas. Você pode escapar das aspas dentro da string usando o caractere de escape de barra invertida (\):
 
+// Corrija a string para que ela use aspas diferentes para o valor href ou escape das existentes. Mantenha as aspas duplas em toda a string.
+
 let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
 console.log(innerHtml);
