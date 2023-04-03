f88a8bce8aa4aed9deabf02839b1bfd9fef580dc
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-03 12:00:56 -0300

    Pegar o uso do operador de atribuição em vez do operador de igualdade

--- debugging/catch-use-of-assignment-operator-instead-of-equality-operator.js
+++ debugging/catch-use-of-assignment-operator-instead-of-equality-operator.js
@@ -2,6 +2,8 @@
 
 // Conforme abordado nos desafios anteriores, o operador de atribuição (=) em JavaScript atribui um valor a um nome de variável.
 
+// E os operadores == e === verificam a igualdade (o triplo === testa a igualdade estrita, o que significa que o valor e o tipo são iguais).
+
 
 
 let x = 7;
