a2a35a48de4c38fded9bc9614fe6dfa97a1b4714
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-04 09:39:10 -0300

    Tenha cuidado ao reinicializar variáveis ​​dentro de um loop

--- debugging/use-caution-when-reinitializing-variables-inside-a-loop.js
+++ debugging/use-caution-when-reinitializing-variables-inside-a-loop.js
@@ -5,22 +5,18 @@
 // Isso é particularmente perigoso se você redefinir acidentalmente a variável que está sendo usada para a condição do terminal, causando um loop infinito.
 
 
-
 function zeroArray(m, n) {
     // Cria uma matriz 2-D com m linhas e n colunas de zeros
     let newArray = [];
     for (let i = 0; i < m; i++) {
 
         let row = [];
-        // Adiciona a m-ésima linha em newArray
-        for (let j = 0; j < n; j++) {
-            
-            // Coloca n zeros na linha atual para criar as colunas
-            // O método push() adiciona um ou mais elementos ao final de um array e retorna o novo comprimento desse array
-            row.push(0);
+
+        for (let j = 0; j < n; j++) {  // Adiciona a m-ésima linha em newArray
+
+            row.push(0);  // Coloca n zeros na linha atual para criar as colunas  // O método push() adiciona um ou mais elementos ao final de um array e retorna o novo comprimento desse array
         }
-        // Empurra a linha atual, que agora possui n zeros, para o array
-        newArray.push(row);
+        newArray.push(row); // Empurra a linha atual, que agora possui n zeros, para o array
     }
     return newArray;
 }
