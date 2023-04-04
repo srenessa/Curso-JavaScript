6c3f05a519346feb1dadb938bcc6e7f8189b048a
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-03 22:20:38 -0300

    Capturar argumentos passados ​​na ordem errada ao chamar uma função

--- debugging/catch-arguments-passed-in-the-wrong-order-when-calling-a-function.js
+++ debugging/catch-arguments-passed-in-the-wrong-order-when-calling-a-function.js
@@ -2,6 +2,8 @@
 
 // O próximo bug a ser observado é quando os argumentos de uma função são fornecidos na ordem incorreta.
 
+// Se os argumentos forem de tipos diferentes, como uma função que espera uma matriz e um número inteiro, isso provavelmente gerará um erro de tempo de execução.
+
 
 function raiseToPower(b, e) {
     return Math.pow(b, e);
