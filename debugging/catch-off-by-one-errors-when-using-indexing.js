04cb2e8c65c00caaa4ac7dbafd456bc8a911e97b
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-04 07:34:11 -0300

    Prenda por um erros ao usar a indexação

--- debugging/catch-off-by-one-errors-when-using-indexing.js
+++ debugging/catch-off-by-one-errors-when-using-indexing.js
@@ -2,6 +2,8 @@
 
 // A indexação JavaScript começa em zero, não em um, o que significa que o último índice é sempre um a menos que o comprimento do item.
 
+// Se você tentar acessar um índice igual ao comprimento, o programa pode lançar um erro de referência "índice fora do intervalo" ou imprimir indefinido.
+
 function countToFive() {
     let firstFive = "12345";
     let len = firstFive.length;
