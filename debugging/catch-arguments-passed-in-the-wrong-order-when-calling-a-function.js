22f27c391d04cc9db414d8bd959b6dd0f06c1129
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-03 22:24:43 -0300

    Capturar argumentos passados ​​na ordem errada ao chamar uma função

--- debugging/catch-arguments-passed-in-the-wrong-order-when-calling-a-function.js
+++ debugging/catch-arguments-passed-in-the-wrong-order-when-calling-a-function.js
@@ -10,9 +10,9 @@
 
 function raiseToPower(b, e) {
     return Math.pow(b, e);
-  }
-  
-  let base = 2;
-  let exp = 3;
-  let power = raiseToPower(base, exp);
-  console.log(power);
+}
+
+let base = 2;
+let exp = 3;
+let power = raiseToPower(base, exp);
+console.log(power);
