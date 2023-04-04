443b2f50158d01206b9dfa1590233d5ea3769ce2
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-04 07:44:46 -0300

    Tenha cuidado ao reinicializar variáveis ​​dentro de um loop

--- debugging/use-caution-when-reinitializing-variables-inside-a-loop.js
+++ debugging/use-caution-when-reinitializing-variables-inside-a-loop.js
@@ -3,18 +3,17 @@ function zeroArray(m, n) {
     let newArray = [];
     let row = [];
     for (let i = 0; i < m; i++) {
-      // Adds the m-th row into newArray
-  
-      for (let j = 0; j < n; j++) {
-        // Pushes n zeroes into the current row to create the columns
-        row.push(0);
-      }
-      // Pushes the current row, which now has n zeroes in it, to the array
-      newArray.push(row);
+        // Adds the m-th row into newArray
+
+        for (let j = 0; j < n; j++) {
+            // Pushes n zeroes into the current row to create the columns
+            row.push(0);
+        }
+        // Pushes the current row, which now has n zeroes in it, to the array
+        newArray.push(row);
     }
     return newArray;
-  }
-  
-  let matrix = zeroArray(3, 2);
-  console.log(matrix);
-  
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
