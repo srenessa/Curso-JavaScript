e2d75ab46de854cf8151b035b731617eff9b66c4
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-03 22:07:35 -0300

    Capturar parênteses de abertura e fechamento ausentes após uma chamada de função

--- debugging/catch-missing-open-and-closing-parenthesis-after-a-function-call.js
+++ debugging/catch-missing-open-and-closing-parenthesis-after-a-function-call.js
@@ -1,4 +1,6 @@
-// Capturar parênteses de abertura e fechamento ausentes após uma chamada de função
+// Capturar parênteses de abertura e fechamento ausentes após uma chamada de função.
+
+// Quando uma função ou método não aceita nenhum argumento, você pode esquecer de incluir os parênteses de abertura e fechamento (vazios) ao chamá-lo.
 
 
 function getNine() {
