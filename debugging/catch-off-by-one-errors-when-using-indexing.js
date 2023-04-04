9484487924d491d255aaf58f40816c2110d27101
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-04 07:41:29 -0300

    Prenda por um erros ao usar a indexação

--- debugging/catch-off-by-one-errors-when-using-indexing.js
+++ debugging/catch-off-by-one-errors-when-using-indexing.js
@@ -4,6 +4,8 @@
 
 // Se você tentar acessar um índice igual ao comprimento, o programa pode lançar um erro de referência "índice fora do intervalo" ou imprimir indefinido.
 
+// Corrija os dois erros de indexação na função a seguir para que todos os números de 1 a 5 sejam impressos no console.
+
 
 function countToFive() {
     let firstFive = "12345";
