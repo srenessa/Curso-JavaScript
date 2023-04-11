ec1251133c6b016630759c21b09907071a74a814
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-11 17:38:56 -0300

    Evite loops infinitos com uma condição de terminal válida

--- debugging/prevent-infinite-loops-with-a-valid-terminal-condition.js
+++ debugging/prevent-infinite-loops-with-a-valid-terminal-condition.js
@@ -1,5 +1,8 @@
 // Evite loops infinitos com uma condição de terminal válida.
 
+// Os loops são ótimas ferramentas quando você precisa que seu programa execute um bloco de código um determinado número de vezes ou até que uma condição seja atendida, mas eles precisam de uma condição terminal que finalize o loop.
+
+
 function myFunc() {
     for (let i = 1; i != 4; i += 2) {
         console.log("Still going!");
