6656d81bbe569eaac764c81640e18113ca2a6169
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-13 18:31:32 -0300

    Use uma matriz para armazenar uma coleção de dados

--- basic-data-structures/use-an-array-to-store-a-collection-of-data.js
+++ basic-data-structures/use-an-array-to-store-a-collection-of-data.js
@@ -1,3 +1,5 @@
 // Use uma matriz para armazenar uma coleção de dados
 
+// Abaixo está um exemplo da implementação mais simples de uma estrutura de dados de matriz.
+
 let yourArray= [ 'Renessa', 'Rafael', 21, 22, true];
