b29536454a700aea9262789a7d266282b88c2fd2
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-13 18:36:57 -0300

    Use uma matriz para armazenar uma coleção de dados

--- basic-data-structures/use-an-array-to-store-a-collection-of-data.js
+++ basic-data-structures/use-an-array-to-store-a-collection-of-data.js
@@ -2,10 +2,12 @@
 
 // Abaixo está um exemplo da implementação mais simples de uma estrutura de dados de matriz.
 
-// let simpleArray = ['one', 2, 'three', true, false, undefined, null];
-// console.log(simpleArray.length);
+/* let simpleArray = ['one', 2, 'three', true, false, undefined, null];
+    console.log(simpleArray.length)*/
 
 // Isso é conhecido como uma matriz unidimensional, o que significa que ela possui apenas um nível ou que não possui nenhuma outra matriz aninhada dentro dela.
 
-let yourArray= [ 'Renessa', 'Rafael', 21, 22, true];
+// Observe que contém booleanos, strings e números, entre outros dados JavaScript válidos
+
+let yourArray = ['Renessa', 'Rafael', 21, 22, true];
 console.log(yourArray.length);
