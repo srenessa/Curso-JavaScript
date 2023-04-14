ebd0fa5daeb6e22d1253060eff39ade65773baf2
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-14 09:18:43 -0300

    Use uma matriz para armazenar uma coleção de dados

--- basic-data-structures/use-an-array-to-store-a-collection-of-data.js
+++ basic-data-structures/use-an-array-to-store-a-collection-of-data.js
@@ -14,27 +14,27 @@
 /*
 let complexArray = [
     [
-       {
+        {
         one: 1,
         two: 2
-      },
-      {
+        },
+        {
         three: 3,
         four: 4
-      }
+        }
     ],
     [
-      {
+        {
         a: "a",
         b: "b"
-      },
-      {
+        },
+        {
         c: "c",
         d: "d"
-      }
+        }
     ]
-  ];
-  
+    ];
+    
 */
 
 // isso é conhecido como uma matriz multidimensional ou uma matriz que contém outras matrizes.
