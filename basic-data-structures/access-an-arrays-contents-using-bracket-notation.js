cd102274125ee9b9353de55308ca9004222ffa37
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-23 12:16:08 -0300

    Acessar o conteúdo de uma matriz usando a notação de colchetes

--- basic-data-structures/access-an-arrays-contents-using-bracket-notation.js
+++ basic-data-structures/access-an-arrays-contents-using-bracket-notation.js
@@ -1,5 +1,7 @@
 // Acessar o conteúdo de uma matriz usando a notação de colchetes
 
+// Agora que aprendemos como criar um array, vamos começar a pensar em como podemos acessar as informações desse array.
+
 let myArray = ["a", "b", "c", "d"];
 // Only change code below this line
 
