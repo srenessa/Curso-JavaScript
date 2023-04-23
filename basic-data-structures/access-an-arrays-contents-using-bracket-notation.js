2d9f6aec89a43af70388c69bbbd850357a2df256
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-23 12:26:53 -0300

    Acessar o conteúdo de uma matriz usando a notação de colchetes

--- basic-data-structures/access-an-arrays-contents-using-bracket-notation.js
+++ basic-data-structures/access-an-arrays-contents-using-bracket-notation.js
@@ -6,7 +6,9 @@
 
 // Para recuperar um elemento de uma matriz, podemos colocar um índice entre colchetes e anexá-lo ao final de uma matriz ou, mais comumente, a uma variável que faz referência a um objeto de matriz.
 
-// Por exemplo, se queremos recuperar o "a" de ourArray e atribuí-lo a uma variável, podemos fazê-lo com o seguinte código: let ourVariable = ourArray[0]; (Agora nossaVariável tem o valor de a.)
+// Por exemplo, se queremos recuperar o "a" de ourArray e atribuí-lo a uma variável, podemos fazê-lo com o seguinte código: let ourVariable = ourArray[0]; (Agora nossaVariável tem o valor de a.).
+
+// Além de acessar o valor associado a um índice, você também pode definir um índice para um valor usando a mesma notação: ourArray[1] = "not b anymore"; (Usando a notação de colchetes, agora redefinimos o item no índice 1 da string b, para não mais b.)
 
 let myArray = ["a", "b", "c", "d"];
 // Only change code below this line
