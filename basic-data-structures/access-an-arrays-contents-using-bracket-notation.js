4d9827bb7e0b77e3a392a8726560709d95d532e2
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-23 12:18:54 -0300

    Acessar o conteúdo de uma matriz usando a notação de colchetes

--- basic-data-structures/access-an-arrays-contents-using-bracket-notation.js
+++ basic-data-structures/access-an-arrays-contents-using-bracket-notation.js
@@ -1,6 +1,8 @@
-// Acessar o conteúdo de uma matriz usando a notação de colchetes
+// Acessar o conteúdo de uma matriz usando a notação de colchetes.
 
 // Agora que aprendemos como criar um array, vamos começar a pensar em como podemos acessar as informações desse array.
+
+// Os arrays JavaScript são indexados em zero, o que significa que o primeiro elemento de um array está realmente na posição zero, não no primeiro.
 
 let myArray = ["a", "b", "c", "d"];
 // Only change code below this line
