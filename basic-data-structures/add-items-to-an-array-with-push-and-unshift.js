3e23315f8b2eee860f895120d9c6c10859994718
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-24 10:55:18 -0300

    Adicionar itens a um array com push() e unshift()

--- basic-data-structures/add-items-to-an-array-with-push-and-unshift.js
+++ basic-data-structures/add-items-to-an-array-with-push-and-unshift.js
@@ -1,5 +1,7 @@
 // Adicionar itens a um array com push() e unshift().
 
+// O comprimento de um array, assim como os tipos de dados que ele pode conter, não é fixo.
+
 function mixedNumbers(arr) {
     // Only change code below this line
 
