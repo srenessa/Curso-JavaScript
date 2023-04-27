f21398458c04eb49e0160cdaf4d1bcfdae3d5af7
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-27 09:24:58 -0300

    Adicionar itens a um array com push() e unshift()

--- basic-data-structures/add-items-to-an-array-with-push-and-unshift.js
+++ basic-data-structures/add-items-to-an-array-with-push-and-unshift.js
@@ -4,7 +4,9 @@
 
 // As matrizes podem ser definidas com um comprimento de qualquer número de elementos e os elementos podem ser adicionados ou removidos ao longo do tempo; ou seja, os arrays são mutáveis.
 
-// O método o método push()  recebem um ou mais elementos como parâmetros e adiciona elementos ao final de uma matriz,
+// O método o método push()  recebem um ou mais elementos como parâmetros e adiciona elementos ao final de uma matriz.
+
+// // O método o método unshift()  recebem um ou mais elementos como parâmetros e adiciona elementos e ao início de uma matriz.
 
 function mixedNumbers(arr) {
     
