13331f4e3b96d24151e29a375de476a86e054f52
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-27 09:42:01 -0300

    Remova itens de um array com pop() e shift()

---
+++ basic-data-structures/remove-items-from-an-array-with-pop-and-shift.js
@@ -0,0 +1,11 @@
+// Remova itens de um array com pop() e shift()
+
+
+
+function popShift(arr) {
+    let popped; // Change this line
+    let shifted; // Change this line
+    return [shifted, popped];
+  }
+  
+  console.log(popShift(['challenge', 'is', 'not', 'complete']));
