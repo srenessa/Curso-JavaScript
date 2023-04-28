915f8999a92dbec11e4c3c4eae3c001f9e743b0e
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-28 17:40:51 -0300

    Remover itens usando splice()

--- basic-data-structures/remove-items-using-splice.js
+++ basic-data-structures/remove-items-using-splice.js
@@ -4,6 +4,8 @@
 
 // splice() pode receber até 3 parâmetros.
 
+// Os dois primeiros parâmetros de splice() são inteiros que representam índices, ou posições, de itens no array para o qual splice() está sendo chamado.
+
 // O primeiro parâmetro de splice() representa o índice no array do qual começar a remover elementos, enquanto o segundo parâmetro indica o número de elementos a serem excluídos.
 
 const arr = [2, 4, 5, 1, 7, 5, 2, 1];
