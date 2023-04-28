d6a5f42d98fe930683b5024c8d4b82ede097a943
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-28 18:25:02 -0300

    Adicionar itens usando splice()

--- basic-data-structures/add-items-using-splice.js
+++ basic-data-structures/add-items-using-splice.js
@@ -2,6 +2,8 @@
 
 // o terceiro parâmetro composto por um ou mais elemento(s) pode ser usado para adicionar ao array.
 
+// Modifique a função usando splice() para remover os dois primeiros elementos da matriz e adicione 'DarkSalmon' e 'BlanchedAlmond' em seus respectivos lugares.
+
 function htmlColorNames(arr) {
     arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond');
 
