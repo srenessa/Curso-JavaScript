bc9454aafd73c48635e3927360461b0b0477849a
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-28 17:11:38 -0300

    Remover itens usando splice()

--- basic-data-structures/remove-items-using-splice.js
+++ basic-data-structures/remove-items-using-splice.js
@@ -1,6 +1,8 @@
-// Remover itens usando splice ()
+// Remover itens usando splice()
 
 // O splice() serve para remover qualquer número de elementos consecutivos de qualquer lugar em um array.
+
+// splice() pode receber até 3 parâmetros
 
 const arr = [2, 4, 5, 1, 7, 5, 2, 1];
 
