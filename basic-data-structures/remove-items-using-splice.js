904137e93886a9c4cd9207f062e944bacb81a536
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-28 17:59:03 -0300

    Remover itens usando splice()

--- basic-data-structures/remove-items-using-splice.js
+++ basic-data-structures/remove-items-using-splice.js
@@ -18,7 +18,10 @@ Aqui removemos 2 elementos, começando com o terceiro elemento (no índice 2). a
 
 // splice() não apenas modifica o array em que está sendo chamado, mas também retorna um novo array contendo o valor dos elementos removidos.
 
+// Use splice() para remover elementos de arr, de modo que ele contenha apenas elementos que somam o valor de 10.
+
 const arr = [2, 4, 5, 1, 7, 5, 2, 1];
 arr.splice(0,4);
 arr.splice(1,1);
+
 console.log(arr);
