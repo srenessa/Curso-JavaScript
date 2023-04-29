bf67e142b962163d6838f2aba1a10ddd6765d834
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-29 19:38:24 -0300

    Copiar itens da matriz usando slice ()

---
+++ basic-data-structures/copy-array-items-using-slice.js
@@ -0,0 +1,8 @@
+function forecast(arr) {
+    // Only change code below this line
+  
+    return arr;
+  }
+  
+  // Only change code above this line
+  console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
