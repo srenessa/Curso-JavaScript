95a273a2e4082ed4cfae08b9bda5e79083009ce6
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-30 11:43:50 -0300

    Copiar uma matriz com o operador Spread

--- basic-data-structures/copy-an-array-with-the-spread-operator.js
+++ basic-data-structures/copy-an-array-with-the-spread-operator.js
@@ -1,9 +1,7 @@
 function copyMachine(arr, num) {
     let newArr = [];
     while (num >= 1) {
-      // Only change code below this line
-  
-      // Only change code above this line
+    
       num--;
     }
     return newArr;
