be9d4a20e0409a5fc554023256f049f3e15112d3
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-30 11:27:03 -0300

    Copiar itens da matriz usando slice ()

--- basic-data-structures/copy-array-items-using-slice.js
+++ basic-data-structures/copy-array-items-using-slice.js
@@ -1,5 +1,7 @@
 // Copiar itens da matriz usando slice ()
 
+// Em vez de modificar um array, slice() copia ou extrai um determinado número de elementos para um novo array, deixando intocado o array para o qual é chamado.
+
 function forecast(arr) {
 
 
