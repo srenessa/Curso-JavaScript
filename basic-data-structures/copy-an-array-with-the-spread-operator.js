a56818a984165eff45ed3651a6ce067ccd4d7c63
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-30 11:46:53 -0300

    Copiar uma matriz com o operador Spread

--- basic-data-structures/copy-an-array-with-the-spread-operator.js
+++ basic-data-structures/copy-an-array-with-the-spread-operator.js
@@ -2,6 +2,8 @@
 
 // O novo operador spread do ES6 nos permite copiar facilmente todos os elementos de um array, em ordem, com uma sintaxe simples e altamente legível.
 
+// A sintaxe de propagação do operador Spread simplesmente se parece com isto: ...
+
 function copyMachine(arr, num) {
     let newArr = [];
     while (num >= 1) {
