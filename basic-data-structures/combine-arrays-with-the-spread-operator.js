922889b1702c30739c0339be201b69a34af48c85
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-30 22:51:19 -0300

    Combine Arrays com o Operador Spread

--- basic-data-structures/combine-arrays-with-the-spread-operator.js
+++ basic-data-structures/combine-arrays-with-the-spread-operator.js
@@ -1,1 +1,3 @@
 // Combine Arrays com o Operador Spread
+
+// Outra grande vantagem do operador spread é a capacidade de combinar arrays, ou inserir todos os elementos de um array em outro, em qualquer índice.
