dbf2c809e5b79a8b6670ef694c4f14215fc93bdb
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-30 22:45:17 -0300

    Copiar uma matriz com o operador Spread

--- basic-data-structures/copy-an-array-with-the-spread-operator.js
+++ basic-data-structures/copy-an-array-with-the-spread-operator.js
@@ -6,6 +6,9 @@
 
 // Definimos uma função, copyMachine, que recebe arr (um array) e num (um número) como argumentos. A função deve retornar uma nova matriz composta de num cópias de arr. Fizemos a maior parte do trabalho para você, mas ainda não está funcionando direito. Modifique a função usando a sintaxe de propagação para que funcione corretamente (dica: outro método que já abordamos pode ser útil aqui!).
 
+// Utilizaremos O método push() para receber duas(num) cópias do arr(array) como parâmetros e adiciona-los essas cópias ao final da matriz.
+
+
 function copyMachine(arr, num) {
     let newArr = [];
     while (num >= 1) {
