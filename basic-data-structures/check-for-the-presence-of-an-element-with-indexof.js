d554f39078ef2cbaae1565a66cd0e6d607e82b22
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-01 10:07:42 -0300

    Verifique a presença de um elemento com indexOf()

--- basic-data-structures/check-for-the-presence-of-an-element-with-indexof.js
+++ basic-data-structures/check-for-the-presence-of-an-element-with-indexof.js
@@ -5,9 +5,7 @@
 // O indexOf nos permite verificar rápida e facilmente a presença de um elemento em um array
 
 function quickCheck(arr, elem) {
-    // Only change code below this line
 
-    // Only change code above this line
 }
 
 console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
