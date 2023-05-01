5a48ed9e2673b74ffa03d3a515d442033d38e8a9
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-01 14:58:37 -0300

    Iterar todos os itens de uma matriz usando loops For

--- basic-data-structures/iterate-through-all-an-arrays-items-using-for-loops.js
+++ basic-data-structures/iterate-through-all-an-arrays-items-using-for-loops.js
@@ -3,9 +3,9 @@
 function filteredArray(arr, elem) {
     let newArr = [];
     // Only change code below this line
-  
+
     // Only change code above this line
     return newArr;
-  }
-  
-  console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
