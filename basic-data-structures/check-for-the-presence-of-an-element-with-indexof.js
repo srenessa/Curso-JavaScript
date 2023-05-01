e9088f7cad7b62f025714421aaa51c745bb6a19c
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-01 09:54:09 -0300

    Verifique a presença de um elemento com indexOf()

--- basic-data-structures/check-for-the-presence-of-an-element-with-indexof.js
+++ basic-data-structures/check-for-the-presence-of-an-element-with-indexof.js
@@ -1,2 +1,9 @@
 // Verifique a presença de um elemento com indexOf()
 
+function quickCheck(arr, elem) {
+    // Only change code below this line
+  
+    // Only change code above this line
+  }
+  
+  console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
