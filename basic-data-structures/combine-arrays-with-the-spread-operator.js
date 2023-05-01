bedd6d880e150debaeaff86aaf7bee5c3375c8a3
Renessa Cerqueira <renessasantos123@gmail.com>
2023-04-30 22:58:02 -0300

    Combine Arrays com o Operador Spread

--- basic-data-structures/combine-arrays-with-the-spread-operator.js
+++ basic-data-structures/combine-arrays-with-the-spread-operator.js
@@ -1,3 +1,12 @@
 // Combine Arrays com o Operador Spread
 
 // Outra grande vantagem do operador spread é a capacidade de combinar arrays, ou inserir todos os elementos de um array em outro, em qualquer índice.
+
+/* EXEMPLO
+A sintaxe de propagação torna a seguinte operação extremamente simples:
+let thisArray = ['sage', 'rosemary', 'parsley', 'thyme'];
+
+let thatArray = ['basil', 'cilantro', ...thisArray, 'coriander'];
+
+thatArray teria o valor ['basil', 'cilantro', 'sage', 'rosemary', 'parsley', 'thyme', 'coriander'].
+*/
