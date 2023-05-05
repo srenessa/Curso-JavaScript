05e70986add109c54c1f6f7792d8d7458e8f3cd4
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-05 18:30:38 -0300

    Iterar todos os itens de uma matriz usando loops For

--- basic-data-structures/iterate-through-all-an-arrays-items-using-for-loops.js
+++ basic-data-structures/iterate-through-all-an-arrays-items-using-for-loops.js
@@ -2,6 +2,8 @@
 
 // A técnica que é mais flexível e nos oferece a maior quantidade de controle é um loop for simples.
 
+// Usando um loop for, essa função itera e acessa cada elemento do array e o submete a um teste simples que criamos.
+
 function filteredArray(arr, elem) {
     let newArr = [];
     
