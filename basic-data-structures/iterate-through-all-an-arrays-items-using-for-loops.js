f2afd9de428c9490b9272c23cdadc3d0a8344ace
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-08 10:40:48 -0300

    Iterar todos os itens de uma matriz usando loops For

--- basic-data-structures/iterate-through-all-an-arrays-items-using-for-loops.js
+++ basic-data-structures/iterate-through-all-an-arrays-items-using-for-loops.js
@@ -4,7 +4,7 @@
 
 // Usando um loop for, essa função itera e acessa cada elemento do array e o submete a um teste simples que criamos.
 
-// Definimos uma função, filterArray, que recebe arr, um array aninhado e elem como argumentos e retorna um novo array. elem representa um elemento que pode ou não estar presente em um ou mais arrays aninhados em arr.
+// Definimos uma função, filterArray, que recebe arr, um array aninhado e elem como argumentos e retorna um novo array. elem representa um elemento que pode ou não estar presente em um ou mais arrays aninhados em arr. Modifique a função, usando um loop for, para retornar uma versão filtrada do array passado de forma que qualquer array aninhado em arr contendo elem tenha sido removido.
 
 function filteredArray(arr, elem) {
     let newArr = [];
