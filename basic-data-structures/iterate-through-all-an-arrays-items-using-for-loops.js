d5f1c5b139883e4d7a5a36c861e9a596493f51db
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-09 11:34:05 -0300

    Iterar todos os itens de uma matriz usando loops For

--- basic-data-structures/iterate-through-all-an-arrays-items-using-for-loops.js
+++ basic-data-structures/iterate-through-all-an-arrays-items-using-for-loops.js
@@ -2,7 +2,7 @@
 
 // O JavaScript oferece vários métodos integrados que iteram sobre arrays de maneiras ligeiramente diferentes para obter resultados diferentes (como every(), forEach(), map(), etc.)
 
-// A técnica que é mais flexível e nos oferece a maior quantidade de controle é um loop for simples.
+// Porém, a técnica que é mais flexível e nos oferece a maior quantidade de controle é um loop for simples.
 
 // Usando um loop for, essa função itera e acessa cada elemento do array e o submete a um teste simples que criamos.
 
