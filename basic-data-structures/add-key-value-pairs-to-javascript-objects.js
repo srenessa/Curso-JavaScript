8e37b293a88c9c7123450ab3b1d525cc439b86b7
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-16 11:27:02 -0300

    Adicionar pares chave-valor a objetos JavaScript

---
+++ basic-data-structures/add-key-value-pairs-to-javascript-objects.js
@@ -0,0 +1,11 @@
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+  };
+  
+  // Only change code below this line
+  
+  // Only change code above this line
+  
+  console.log(foods);
