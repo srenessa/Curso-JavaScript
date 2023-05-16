23e15a41acf55afaaedc881230a67e959acf813c
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-16 11:23:22 -0300

    Crie matrizes multidimensionais complexas

--- basic-data-structures/create-complex-multi-dimensional-arrays.js
+++ basic-data-structures/create-complex-multi-dimensional-arrays.js
@@ -6,16 +6,19 @@
 
 // Dessa forma, um array pode rapidamente se tornar uma estrutura de dados muito complexa, conhecida como array multidimensional ou aninhado.
 
-// A matriz profunda é aninhada em 2 níveis de profundidade. As matrizes mais profundas têm 3 níveis de profundidade. As matrizes mais profundas são de 4 níveis de profundidade e assim por diante.
+//A matriz profunda é aninhada em 2 níveis de profundidade(deep). As matrizes mais profundas têm 3 níveis de profundidade(deeper). As matrizes mais profundas são de 4 níveis(deepest) e as mais profundas? é 5(deepest-est).
 
 // Podemos acessar facilmente os níveis mais profundos de uma matriz tão complexa com a notação de colchetes: console.log(nestedArray[2][1][0][0][0]);
 // E agora que sabemos onde está esse dado, podemos redefini-lo se precisarmos: nestedArray[2][1][0][0][0] = 'deeper still';
 
 let myNestedArray = [
+
     ['unshift', false, 1, 2, 3, 'complex', 'nested'],
     ['loop', 'shift', 6, 7, 1000, 'method'],
     ['concat', false, true, 'spread', 'array', ['deep']],
     ['mutate', 1327.98, 'splice', 'slice', 'push', [['deeper']]],
     ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth', [[['deepest']]]]
 
-  ];
+];
+
+console.log(myNestedArray);
