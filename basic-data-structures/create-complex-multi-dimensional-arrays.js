801e0e88725d42a8e48e602e153972329d0b0c06
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-16 11:17:14 -0300

    Crie matrizes multidimensionais complexas

--- basic-data-structures/create-complex-multi-dimensional-arrays.js
+++ basic-data-structures/create-complex-multi-dimensional-arrays.js
@@ -12,11 +12,10 @@
 // E agora que sabemos onde está esse dado, podemos redefini-lo se precisarmos: nestedArray[2][1][0][0][0] = 'deeper still';
 
 let myNestedArray = [
-    // Only change code below this line
     ['unshift', false, 1, 2, 3, 'complex', 'nested'],
     ['loop', 'shift', 6, 7, 1000, 'method'],
     ['concat', false, true, 'spread', 'array', ['deep']],
     ['mutate', 1327.98, 'splice', 'slice', 'push', [['deeper']]],
     ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth', [[['deepest']]]]
-    // Only change code above this line
+
   ];
