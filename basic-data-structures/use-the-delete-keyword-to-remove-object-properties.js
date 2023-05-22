8ccacf68489726dfb241e47ddd210b5268143e2c
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-22 12:48:35 -0300

    Use a palavra-chave delete para remover as propriedades do objeto

--- basic-data-structures/use-the-delete-keyword-to-remove-object-properties.js
+++ basic-data-structures/use-the-delete-keyword-to-remove-object-properties.js
@@ -5,3 +5,19 @@
 // Em desafios anteriores, adicionamos e modificamos os pares chave-valor de um objeto. Aqui veremos como podemos remover um par chave-valor de um objeto.
 
 // Para remover deve-se utilizar o comando delete, seguido do nome do objeto e por fim utilizaremos a propriedade que queremos remover do objeto. Por exemplo: delete foods.apples;
+
+
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+  
+  // Only change code below this line
+  
+  // Only change code above this line
+  
+  console.log(foods);
