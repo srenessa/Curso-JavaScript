55994f95640a11ad6801e4f418de77d9f54b197b
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-22 11:53:27 -0300

    Modificar um objeto aninhado dentro de um objeto

--- basic-data-structures/modify-an-object-nested-within-an-object.js
+++ basic-data-structures/modify-an-object-nested-within-an-object.js
@@ -1,1 +1,3 @@
 // Modificar um objeto aninhado dentro de um objeto
+
+// As propriedades do objeto podem ser aninhadas em uma profundidade arbitrária e seus valores podem ser qualquer tipo de dados suportados pelo JavaScript, incluindo matrizes e até mesmo outros objetos.
