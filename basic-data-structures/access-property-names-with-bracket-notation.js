9fa8bd5191400cf305917df513f6daa2cf2f0d06
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-22 12:26:49 -0300

    Acessar nomes de propriedades com notação de colchetes

--- basic-data-structures/access-property-names-with-bracket-notation.js
+++ basic-data-structures/access-property-names-with-bracket-notation.js
@@ -1,3 +1,5 @@
 // Acessar nomes de propriedades com notação de colchetes
 
 // No primeiro desafio de objeto, mencionamos o uso da notação de colchetes como forma de acessar valores de propriedade usando a avaliação de uma variável.
+
+// A notação de colchetes é muito útil porque às vezes as propriedades do objeto não são conhecidas antes do tempo de execução ou precisamos acessá-las de maneira mais dinâmica.
