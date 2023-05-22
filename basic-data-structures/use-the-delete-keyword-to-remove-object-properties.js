72037eb8b9fd4cbe326b3afb9397a597402a83a6
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-22 12:39:38 -0300

    Use a palavra-chave delete para remover as propriedades do objeto

--- basic-data-structures/use-the-delete-keyword-to-remove-object-properties.js
+++ basic-data-structures/use-the-delete-keyword-to-remove-object-properties.js
@@ -1,1 +1,3 @@
 // Use a palavra-chave delete para remover as propriedades do objeto
+
+// Os objetos são armazenamentos de valor-chave que fornecem uma maneira flexível e intuitiva de estruturar dados e fornecem um tempo de pesquisa muito rápido.
