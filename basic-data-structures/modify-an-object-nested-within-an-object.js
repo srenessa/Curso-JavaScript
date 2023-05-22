6b5e6da9519c7ed03e664c96eff2442b9762b5c7
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-22 12:14:35 -0300

    Modificar um objeto aninhado dentro de um objeto

--- basic-data-structures/modify-an-object-nested-within-an-object.js
+++ basic-data-structures/modify-an-object-nested-within-an-object.js
@@ -2,17 +2,26 @@
 
 // As propriedades do objeto podem ser aninhadas em uma profundidade arbitrária e seus valores podem ser qualquer tipo de dados suportados pelo JavaScript, incluindo matrizes e até mesmo outros objetos.
 
-// Para atribuir o valor 10 à propriedade ocupada do objeto onlineStatus aninhado, usamos a notação de ponto para referenciar a propriedade.
+// Para atribuir o valor 10 à propriedade ocupada do objeto onlineStatus(busy) aninhado, usamos a notação de ponto para referenciar a propriedade.
 
 /* antes: 
-  data: {
+let nestedObject = {
+    id: 28802695164,
+    date: 'December 31, 2016',
+data: {
     totalUsers: 99,
     online: 80,
     onlineStatus: {
-      active: 67,
-      away: 13,
-      busy: 8
+    active: 67,
+    away: 13,
+    busy: 8
     }
+        }
+};
 
 durante:
 nestedObject.data.onlineStatus.busy = 10;
+*/
+
+// Aqui definimos um objeto userActivity, que inclui outro objeto aninhado dentro dele. Defina o valor da chave online para 45.
+
