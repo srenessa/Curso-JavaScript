316129fd462a4b7e5c67ce4d37901d333184df38
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-23 12:36:35 -0300

    Verifique se um objeto tem uma propriedade

--- basic-data-structures/check-if-an-object-has-a-property.js
+++ basic-data-structures/check-if-an-object-has-a-property.js
@@ -3,3 +3,10 @@
 // Agora podemos adicionar, modificar e remover chaves de objetos. Mas e se apenas quiséssemos saber se um objeto tem uma propriedade específica?
 
 // O JavaScript nos fornece duas maneiras diferentes de fazer isso. Um usa o método hasOwnProperty() e o outro usa a palavra-chave in. 
+
+/* Por exemplo se tivermos um objeto users com uma propriedade de Alan, podemos verificar sua presença de uma das seguintes maneiras:
+
+users.hasOwnProperty('Alan');
+'Alan' in users;
+
+*/
