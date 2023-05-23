3332b17d0f1c4288f351a42dcdbeeeca52f94912
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-23 12:17:44 -0300

    Verifique se um objeto tem uma propriedade

--- basic-data-structures/check-if-an-object-has-a-property.js
+++ basic-data-structures/check-if-an-object-has-a-property.js
@@ -1,1 +1,3 @@
 // Verifique se um objeto tem uma propriedade
+
+// Agora podemos adicionar, modificar e remover chaves de objetos. Mas e se apenas quiséssemos saber se um objeto tem uma propriedade específica?
