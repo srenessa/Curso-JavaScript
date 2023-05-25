9b8ce2104150b196bd750f662581ac2ca6b1c683
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-25 10:53:54 -0300

    Iterar pelas chaves de um objeto com uma instrução for...in

--- basic-data-structures/iterate-through-the-keys-of-an-object-with-a-for---in-statement.js
+++ basic-data-structures/iterate-through-the-keys-of-an-object-with-a-for---in-statement.js
@@ -1,3 +1,23 @@
 // Iterar pelas chaves de um objeto com uma instrução for...in
 
 // Às vezes, você precisa percorrer todas as chaves de um objeto. Você pode usar um loop for...in para fazer isso.
+
+const users = {
+    Alan: {
+      online: false
+    },
+    Jeff: {
+      online: true
+    },
+    Sarah: {
+      online: false
+    }
+  }
+  
+  function countOnline(allUsers) {
+    // Only change code below this line
+  
+    // Only change code above this line
+  }
+  
+  console.log(countOnline(users));
