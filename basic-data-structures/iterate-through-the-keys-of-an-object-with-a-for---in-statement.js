90a59d2a795de4589114b4b0edd7468a12c00dbf
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-25 18:29:41 -0300

    Iterar pelas chaves de um objeto com uma instrução for...in

--- basic-data-structures/iterate-through-the-keys-of-an-object-with-a-for---in-statement.js
+++ basic-data-structures/iterate-through-the-keys-of-an-object-with-a-for---in-statement.js
@@ -4,7 +4,7 @@
 
 // NOTA: Os objetos não mantêm uma ordem para as chaves armazenadas como os arrays; portanto, a posição de uma chave em um objeto, ou a ordem relativa em que ela aparece, é irrelevante ao referenciar ou acessar essa chave.
 
-// Definimos uma função countOnline que aceita um argumento, allUsers.
+// Definimos uma função countOnline que aceita um argumento, allUsers.Use uma instrução for...in dentro dessa função para percorrer o objeto allUsers e retornar o número de usuários cuja propriedade online está definida como true.
 
 const users = {
     Alan: {
