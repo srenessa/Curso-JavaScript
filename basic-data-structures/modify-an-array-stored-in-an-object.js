e020047510f70d233c989ae66253bdd39cb71539
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-29 15:45:13 -0300

    Modificar uma matriz armazenada em um objeto

--- basic-data-structures/modify-an-array-stored-in-an-object.js
+++ basic-data-structures/modify-an-array-stored-in-an-object.js
@@ -2,6 +2,8 @@
 
 // Agora você já viu todas as operações básicas para objetos JavaScript. Você pode adicionar, modificar e remover pares chave-valor, verificar se existem chaves e iterar todas as chaves em um objeto.
 
+// O objeto de usuário contém três chaves. A chave de dados contém cinco chaves, uma das quais contém uma matriz de amigos.
+
 
 let user = {
     name: 'Kenneth',
