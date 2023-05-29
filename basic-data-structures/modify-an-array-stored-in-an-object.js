368079a43e41b1501ed9d2e199538df08951046d
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-29 15:32:13 -0300

    Modificar uma matriz armazenada em um objeto

--- basic-data-structures/modify-an-array-stored-in-an-object.js
+++ basic-data-structures/modify-an-array-stored-in-an-object.js
@@ -5,26 +5,26 @@ let user = {
     name: 'Kenneth',
     age: 28,
     data: {
-      username: 'kennethCodesAllDay',
-      joinDate: 'March 26, 2016',
-      organization: 'freeCodeCamp',
-      friends: [
-        'Sam',
-        'Kira',
-        'Tomo'
-      ],
-      location: {
-        city: 'San Francisco',
-        state: 'CA',
-        country: 'USA'
-      }
+        username: 'kennethCodesAllDay',
+        joinDate: 'March 26, 2016',
+        organization: 'freeCodeCamp',
+        friends: [
+            'Sam',
+            'Kira',
+            'Tomo'
+        ],
+        location: {
+            city: 'San Francisco',
+            state: 'CA',
+            country: 'USA'
+        }
     }
-  };
-  
-  function addFriend(userObj, friend) {
+};
+
+function addFriend(userObj, friend) {
     // Only change code below this line
-  
+
     // Only change code above this line
-  }
-  
-  console.log(addFriend(user, 'Pete'));
+}
+
+console.log(addFriend(user, 'Pete'));
