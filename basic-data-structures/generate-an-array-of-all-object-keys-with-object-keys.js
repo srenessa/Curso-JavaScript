323206efeb6f04f11643ddef00058fa220b91b03
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-29 15:21:23 -0300

    Gere uma matriz de todas as chaves de objeto com Object.keys()

--- basic-data-structures/generate-an-array-of-all-object-keys-with-object-keys.js
+++ basic-data-structures/generate-an-array-of-all-object-keys-with-object-keys.js
@@ -3,27 +3,27 @@
 
 let users = {
     Alan: {
-      age: 27,
-      online: false
+        age: 27,
+        online: false
     },
     Jeff: {
-      age: 32,
-      online: true
+        age: 32,
+        online: true
     },
     Sarah: {
-      age: 48,
-      online: false
+        age: 48,
+        online: false
     },
     Ryan: {
-      age: 19,
-      online: true
+        age: 19,
+        online: true
     }
-  };
-  
-  function getArrayOfUsers(obj) {
+};
+
+function getArrayOfUsers(obj) {
     // Only change code below this line
-  
+
     // Only change code above this line
-  }
-  
-  console.log(getArrayOfUsers(users));
+}
+
+console.log(getArrayOfUsers(users));
