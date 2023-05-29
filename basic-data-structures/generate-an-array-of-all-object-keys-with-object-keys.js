161f8e15a59bd4f8a351ab383b737e0cf97958a4
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-29 15:23:02 -0300

    Gere uma matriz de todas as chaves de objeto com Object.keys()

--- basic-data-structures/generate-an-array-of-all-object-keys-with-object-keys.js
+++ basic-data-structures/generate-an-array-of-all-object-keys-with-object-keys.js
@@ -2,6 +2,8 @@
 
 // Também podemos gerar um array que contém todas as chaves armazenadas em um objeto com o método Object.keys().
 
+// Esse método usa um objeto como argumento e retorna uma matriz de strings representando cada propriedade no objeto.
+
 
 let users = {
     Alan: {
