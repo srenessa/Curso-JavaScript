c93850a69833feeb73b1f7f3b0e6d250251ffc96
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-30 10:30:17 -0300

    Converter Celsius para Fahrenheit

--- basic-algorithm-scripting/convert-celsius-to-fahrenheit.js
+++ basic-algorithm-scripting/convert-celsius-to-fahrenheit.js
@@ -1,3 +1,5 @@
 // Converter Celsius para Fahrenheit
 
 // A fórmula para converter de Celsius para Fahrenheit é a temperatura em Celsius vezes 9/5, mais 32.
+
+// Você recebe uma variável celsius que representa uma temperatura em Celsius. Use a variável Fahrenheit já definida e atribua a ela a temperatura Fahrenheit equivalente à temperatura Celsius fornecida.
