b36dfbd90552167f5fca663ab049f01ba597a59f
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-31 11:06:59 -0300

    Fatorializar um Número

--- basic-algorithm-scripting/factorialize-a-number.js
+++ basic-algorithm-scripting/factorialize-a-number.js
@@ -1,3 +1,5 @@
 // Fatorializar um Número
 
 // Retorna o fatorial do inteiro fornecido.
+
+// Se o inteiro for representado pela letra n, um fatorial é o produto de todos os inteiros positivos menores ou iguais a n.
