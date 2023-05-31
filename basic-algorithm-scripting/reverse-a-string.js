3909f716b53f7ad62eca7290a16860c27339bb9b
Renessa Cerqueira <renessasantos123@gmail.com>
2023-05-31 10:56:11 -0300

    Inverter uma String

--- basic-algorithm-scripting/reverse-a-string.js
+++ basic-algorithm-scripting/reverse-a-string.js
@@ -3,7 +3,18 @@
 // Inverta a string fornecida e retorne a string invertida.
 
 function reverseString(str) {
-    return str;
+    // Inicializa a nova string invertida
+    var reversedString = '';
+  
+    // Percorre a string de trás para frente
+    for (var i = str.length - 1; i >= 0; i--) {
+      // Adiciona cada caractere à nova string
+      reversedString += str[i];
+    }
+  
+    // Retorna a string invertida
+    return reversedString;
   }
   
-  reverseString("hello");
+  reverseString("hello");  // Retorna "olleh"
+  
