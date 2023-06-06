3947b7947efefb43580b445121e6000d77038950
Renessa Cerqueira <renessasantos123@gmail.com>
2023-06-06 17:33:34 -0300

    inverter uma string versao dois

---
+++ basic-algorithm-scripting/reverse-a-string-versao-dois.js
@@ -0,0 +1,17 @@
+// 1. A função reverseString é definida, que recebe uma string str como parâmetro.
+
+// 2. Uma variável reversedString é inicializada como uma string vazia. Essa variável será usada para construir a nova string invertida.
+
+// 3.
+
+// 4.
+
+
+
+function reverseString(str) {
+    let reversedStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+    }
+    return reversedStr;
+}
