05f05053b90c982f0c5786c21b5489bee63c27fe
Renessa Cerqueira <renessasantos123@gmail.com>
2023-06-06 17:34:01 -0300

    Update reverse-a-string-versao-dois.js

--- basic-algorithm-scripting/reverse-a-string-versao-dois.js
+++ basic-algorithm-scripting/reverse-a-string-versao-dois.js
@@ -2,7 +2,7 @@
 
 // 2. Uma variável reversedString é inicializada como uma string vazia. Essa variável será usada para construir a nova string invertida.
 
-// 3.
+// 3. O próximo passo é percorrer a string str de trás para frente. Para isso, é utilizado um loop for com uma variável i que começa no último índice da string (str.length - 1) e decrementa a cada iteração até chegar a 0 (i >= 0).
 
 // 4.
 
