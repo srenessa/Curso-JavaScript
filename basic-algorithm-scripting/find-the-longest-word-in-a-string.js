df5d22fdcc54c976cd32421031f0a21773638f06
Renessa Cerqueira <renessasantos123@gmail.com>
2023-06-11 15:15:27 -0300

    Encontre a palavra mais longa em uma string

--- basic-algorithm-scripting/find-the-longest-word-in-a-string.js
+++ basic-algorithm-scripting/find-the-longest-word-in-a-string.js
@@ -1,3 +1,10 @@
 // Encontre a palavra mais longa em uma string
 
 // Retorna o comprimento da palavra mais longa na frase fornecida. Sua resposta deve ser um número.
+
+
+function findLongestWordLength(str) {
+    return str.length;
+  }
+  
+  findLongestWordLength("The quick brown fox jumped over the lazy dog");
