1ee35421f8e521d35fb68231bc1ed97161063489
Renessa Cerqueira <renessasantos123@gmail.com>
2023-06-21 15:43:25 -0300

    Retornar os maiores números em matrizes

--- basic-algorithm-scripting/return-largest-numbers-in-arrays.js
+++ basic-algorithm-scripting/return-largest-numbers-in-arrays.js
@@ -5,8 +5,21 @@
 // Lembre-se, você pode iterar por meio de uma matriz com um loop for simples e acessar cada membro com a sintaxe da matriz arr[i].
 
 
-function largestOfFour(arr) {
-    return arr;
+function maiorDeQuatro(arr) {
+    let resultMaiorNumero = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let maiorNumero = arr[i][0];
+
+        for (let j = 1; j < arr[i].length; j++) {
+            if (arr[i][j] > maiorNumero) {
+                maiorNumero = arr[i][j];
+            }
+        }
+
+        resultMaiorNumero[i] = maiorNumero;
+    }
+    return resultMaiorNumero;
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+maiorNumero([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
