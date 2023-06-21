156b21202ada4e5c89628be5d5dc465906067f93
Renessa Cerqueira <renessasantos123@gmail.com>
2023-06-21 14:30:00 -0300

    Retornar os maiores números em matrizes

--- basic-algorithm-scripting/return-largest-numbers-in-arrays.js
+++ basic-algorithm-scripting/return-largest-numbers-in-arrays.js
@@ -1,1 +1,5 @@
 // Retornar os maiores números em matrizes
+
+// Retorna um array que consiste no maior número de cada sub-array fornecido. Para simplificar, o array fornecido conterá exatamente 4 sub-arrays.
+
+// Lembre-se, você pode iterar por meio de uma matriz com um loop for simples e acessar cada membro com a sintaxe da matriz arr[i].
