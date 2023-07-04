fc545fe84497e2566a701d71467ece5bb7c6376d
Renessa Cerqueira <renessasantos123@gmail.com>
2023-07-04 16:59:49 -0300

    Truncar uma String

--- basic-algorithm-scripting/truncate-a-string.js
+++ basic-algorithm-scripting/truncate-a-string.js
@@ -1,7 +1,26 @@
 // Truncar uma String
 
+// significado: retirar uma parte de/ separar
+
+// No javascript por exemplo, retorna a parte inteira de um número, descartando suas casas decimais
+
+// Truncar uma string (primeiro argumento) se ela for maior que o comprimento máximo de string fornecido (segundo argumento). Retorne a string truncada com um final ....
+
+
+// Essa função verifica se a string excede o comprimento máximo e se for necessário, trunca a string e adiciona "..." ao final. 
+
 function truncateString(str, num) {
+    if (str.length > num) {
+
+        return str.slice(0, num) + "...";
+
+    }
     return str;
+    // resolveria também com a utilização do else{ return str;}
+
 }
 
-truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 10));
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length));
