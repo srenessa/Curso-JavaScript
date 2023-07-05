d18eb5013082084391370bd0b9328f4f99a5af64
Renessa Cerqueira <renessasantos123@gmail.com>
2023-07-05 17:28:15 -0300

    atualização do código

--- basic-algorithm-scripting/finders-keepers.js
+++ basic-algorithm-scripting/finders-keepers.js
@@ -1,6 +1,17 @@
 function findElement(arr, func) {
     let num = 0;
-    return num;
+
+    for (let i = 0; i < arr.length; i++) {
+        num = arr[i];
+
+        if (func(num)) {
+            return num;
+        }
+    }
+    return undefined;
 }
 
-findElement([1, 2, 3, 4], num => num % 2 === 0);
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
+
+
+// Crie uma função que examine um array arr e retorne o primeiro elemento nele que passe em um 'teste de verdade'. Isso significa que, dado um elemento x, o 'teste de verdade' é aprovado se func(x) for verdadeiro. Se nenhum elemento passar no teste, retorna indefinido.
