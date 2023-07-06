552262f07c1d90881a54bb57e84ecb01fe3335a3
Renessa Cerqueira <renessasantos123@gmail.com>
2023-07-06 11:27:37 -0300

    Verifique se um valor é classificado como um primitivo booleano.

--- basic-algorithm-scripting/boo-who.js
+++ basic-algorithm-scripting/boo-who.js
@@ -1,7 +1,8 @@
+// Nessa função, utilizamos o operador typeof para verificar se o tipo de value é igual a 'boolean'. Se for igual, a função retorna true, caso contrário, retorna false.
+
 function booWho(bool) {
 
     return typeof bool === "boolean";
 }
-
 
 booWho(null);
