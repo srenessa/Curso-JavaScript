1349a8b0becd33424a59329988de2af254b8ebe6
Renessa Cerqueira <renessasantos123@gmail.com>
2023-07-17 16:56:33 -0300

    Title Case a Sentence

--- basic-algorithm-scripting/title-case-a-sentence.js
+++ basic-algorithm-scripting/title-case-a-sentence.js
@@ -1,5 +1,14 @@
 function titleCase(str) {
-    return str;
+    const palavras = titleCase.split("");
+
+    for(let i = 0; i < palavras.length;i++ ){
+        palavras[i]= palavras[i][0].toUpperCase();
+
+    }
 }
 
-titleCase("I'm a little tea pot");
+console.log(titleCase("I'm a little tea pot"));
+
+// separar as palavras com split
+// utilizar um for
+// utilizamos o método toUpperCase() para converter uma string em letras maiúsculas.
