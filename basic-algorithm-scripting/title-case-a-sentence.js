a3d6281988d5af10580198b8d8ddc94f6694bb44
Renessa Cerqueira <renessasantos123@gmail.com>
2023-08-16 16:46:45 -0300

    atualização de código

--- basic-algorithm-scripting/title-case-a-sentence.js
+++ basic-algorithm-scripting/title-case-a-sentence.js
@@ -2,16 +2,12 @@
 // Title Case a Sentence
 
 function titleCase(str) {
-    const palavras = titleCase.split(" ");
+    const palavras = str.split(" ");
 
-    for(let i = 0; i < palavras.length; i++ ){
-        palavras[i]= palavras[i][0].toUpperCase() + palavras[i].substr(1);
-
-    }
-    palavras.join(" ");
+    console.log(palavras);
 }
 
-console.log(titleCase("I'm a little tea pot"));
+titleCase("I'm a little tea pot");
 
 // Retorne a string fornecida com a primeira letra de cada palavra em maiúscula. Certifique-se de que o restante da palavra esteja em letras minúsculas. Para o propósito deste exercício, você também deve colocar palavras de ligação em maiúsculas como o e de.
 
