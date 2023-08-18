0de98de577a57efccffbcbb498433d791ab49330
Renessa Cerqueira <renessasantos123@gmail.com>
2023-08-18 11:58:47 -0300

    Corte e Emenda

--- basic-algorithm-scripting/slice-and-splice.js
+++ basic-algorithm-scripting/slice-and-splice.js
@@ -5,3 +5,10 @@ function frankenSplice(arr1, arr2, n) {
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+
+
+// Você recebe dois arrays e um índice.
+// Copie cada elemento da primeira matriz para a segunda matriz, em ordem.
+// Comece inserindo elementos no índice n da segunda matriz.
+// Retorne a matriz resultante. As matrizes de entrada devem permanecer as mesmas após a execução da função.
