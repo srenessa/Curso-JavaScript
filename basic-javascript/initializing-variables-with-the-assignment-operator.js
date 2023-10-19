77364ecbb1fdca395763ba612c06797e8e59268a
Renessa Cerqueira <renessasantos123@gmail.com>
2023-10-19 16:29:35 -0300

    Inicializando variáveis ​​com o operador de atribuição

--- basic-javascript/initializing-variables-with-the-assignment-operator.js
+++ basic-javascript/initializing-variables-with-the-assignment-operator.js
@@ -1,1 +1,7 @@
+// Inicializando variáveis ​​com o operador de atribuição
+
+// É comum inicializar uma variável com um valor inicial na mesma linha em que ela é declarada.
+
+// Defina uma variável a com var e inicialize-a com um valor de 9.
+
 var a =9;
