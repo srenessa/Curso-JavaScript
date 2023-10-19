8deccc5891ea4735812790a3b0370d8e315632d5
Renessa Cerqueira <renessasantos123@gmail.com>
2023-10-19 10:55:48 -0300

    Declarar variáveis ​​JavaScript

--- basic-javascript/declare-javascript-variables.js
+++ basic-javascript/declare-javascript-variables.js
@@ -1,4 +1,6 @@
 // Declarar variáveis ​​JavaScript
+
 // Em JavaScript, var é uma palavra-chave usada para declarar uma variável. Quando você usa var, você está criando uma nova variável que pode armazenar um valor. 
+// Os nomes das variáveis ​​podem ser compostos por números, letras e $ ou _, mas não podem conter espaços ou começar com um número.
 
 var myName;
