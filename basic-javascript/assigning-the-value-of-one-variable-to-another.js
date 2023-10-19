8fa8e35367fd0edbccf7dc0cdd407232e88155f6
Renessa Cerqueira <renessasantos123@gmail.com>
2023-10-19 16:12:33 -0300

    Atribuindo o valor de uma variável a outra

--- basic-javascript/assigning-the-value-of-one-variable-to-another.js
+++ basic-javascript/assigning-the-value-of-one-variable-to-another.js
@@ -1,8 +1,12 @@
-// Setup
+// Atribuindo o valor de uma variável a outra
+
+// Depois que um valor é atribuído a uma variável usando o operador de atribuição, você pode atribuir o valor dessa variável a outra variável usando o operador de atribuição.
 var a;
 a = 7;
 var b;
 
-// Only change code below this line
+
 
 b = a;
+
+// A variável 'a' é declarada sem valor e, em seguida, é atribuido a ela o valor 7. Em seguida, uma variável chamada 'b' é declarada sem valor. Então, o conteúdo de 'a' (que é 7) é atribuído à variável b. Agora, b também tem o valor 7.
