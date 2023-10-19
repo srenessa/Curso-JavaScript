5616c319f04761ed3b4505c856707c464bf07a27
Renessa Cerqueira <renessasantos123@gmail.com>
2023-10-19 14:51:58 -0300

    Armazenando Valores com o Operador de Atribuição

--- basic-javascript/storing-values-with-the-assignment-operator.js
+++ basic-javascript/storing-values-with-the-assignment-operator.js
@@ -1,5 +1,8 @@
-// Setup
+// Armazenando Valores com o Operador de Atribuição
+
+// Primeiro, este código cria uma variável chamada a.
 var a;
 
-// Only change code below this line
+// Atribua o valor 7 à variável a.
+// Então, o código atribui 7 a 'a'. Agora, se 'a' aparecer novamente no código, o programa irá tratá-la como se fosse 7.
 a =7;
