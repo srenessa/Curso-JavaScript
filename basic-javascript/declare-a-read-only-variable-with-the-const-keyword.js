03c27ae6143aac641dc4a06853dff80285694a46
Renessa Cerqueira <renessasantos123@gmail.com>
2023-11-14 17:35:26 -0300

    Declare uma variável somente leitura com a palavra-chave const

--- basic-javascript/declare-a-read-only-variable-with-the-const-keyword.js
+++ basic-javascript/declare-a-read-only-variable-with-the-const-keyword.js
@@ -1,3 +1,13 @@
+// Declare uma variável somente leitura com a palavra-chave const
+
+// As variáveis ​​declaradas usando const são somente leitura. Eles são um valor constante, o que significa que uma vez que uma variável é atribuída com const, ela não pode ser reatribuída.
+
+// Você deve sempre nomear variáveis ​​que não deseja reatribuir usando a palavra-chave const.
+
+// Nota: É comum que os desenvolvedores usem identificadores de variáveis ​​em letras maiúsculas para valores imutáveis ​​e letras minúsculas ou camelCase.
+
+// Altere o código para que todas as variáveis ​​sejam declaradas usando let ou const. Use let quando quiser que a variável mude e const quando quiser que a variável permaneça constante. Além disso, renomeie as variáveis ​​declaradas com const para estar em conformidade com as práticas comuns. Não altere as strings atribuídas às variáveis.
+
 const FCC = "freeCodeCamp"; // Change this line
 let fact = "is cool!"; // Change this line
 fact = "is awesome!";
