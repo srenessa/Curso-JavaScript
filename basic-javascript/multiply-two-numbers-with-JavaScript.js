773a02ff70e82243b7f0d2fe7bf0040d2eef1354
Renessa Cerqueira <renessasantos123@gmail.com>
2023-11-14 17:53:13 -0300

    Multiplique dois números com JavaScript

--- basic-javascript/multiply-two-numbers-with-JavaScript.js
+++ basic-javascript/multiply-two-numbers-with-JavaScript.js
@@ -1,1 +1,7 @@
+// Multiplique dois números com JavaScript
+
+// JavaScript usa o símbolo * para multiplicação de dois números.
+
+// Mude o 0 para que o produto seja igual a 80.
+
 const product = 8 * 10;
