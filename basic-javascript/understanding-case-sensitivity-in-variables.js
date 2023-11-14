3ffa029b46bef84094b15c95bfe1fe8087db43b2
Renessa Cerqueira <renessasantos123@gmail.com>
2023-11-14 10:43:54 -0300

    Compreendendo a distinção entre maiúsculas e minúsculas em variáveis

--- basic-javascript/understanding-case-sensitivity-in-variables.js
+++ basic-javascript/understanding-case-sensitivity-in-variables.js
@@ -1,3 +1,11 @@
+// Compreendendo a distinção entre maiúsculas e minúsculas em variáveis
+
+// MYVAR não é o mesmo que MyVar nem myvar. É possível ter múltiplas variáveis ​​distintas com o mesmo nome, mas com maiúsculas e minúsculas diferentes
+
+// Escreva nomes de variáveis ​​em JavaScript em camelCase. No camelCase, nomes de variáveis ​​com várias palavras têm a primeira palavra em minúscula e a primeira letra de cada palavra subsequente é maiúscula.
+
+// Modifique as declarações e atribuições existentes para que seus nomes usem camelCase. 
+
 // Variable declarations
 var studlyCapVar;
 var properCamelCase;
