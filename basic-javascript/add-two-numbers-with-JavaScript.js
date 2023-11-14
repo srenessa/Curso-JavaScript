744a0fbb4e989924762a2af0df52cae77b05078b
Renessa Cerqueira <renessasantos123@gmail.com>
2023-11-14 17:43:33 -0300

    Adicione dois números com JavaScript

--- basic-javascript/add-two-numbers-with-JavaScript.js
+++ basic-javascript/add-two-numbers-with-JavaScript.js
@@ -1,1 +1,9 @@
+// Adicione dois números com JavaScript
+
+// Number é um tipo de dados em JavaScript que representa dados numéricos.
+
+// JavaScript usa o símbolo + como operador de adição quando colocado entre dois números.
+
+// Mude o 0 para que a soma seja igual a 20.
+
 const sum = 10 + 10;
--- basic-javascript/declare-a-read-only-variable-with-the-const-keyword.js
+++ basic-javascript/declare-a-read-only-variable-with-the-const-keyword.js
@@ -8,7 +8,7 @@
 
 // Altere o código para que todas as variáveis ​​sejam declaradas usando let ou const. Use let quando quiser que a variável mude e const quando quiser que a variável permaneça constante. Além disso, renomeie as variáveis ​​declaradas com const para estar em conformidade com as práticas comuns. Não altere as strings atribuídas às variáveis.
 
-const FCC = "freeCodeCamp"; // Change this line
-let fact = "is cool!"; // Change this line
+const FCC = "freeCodeCamp"; 
+let fact = "is cool!"; 
 fact = "is awesome!";
-console.log(FCC, fact); // Change this line  
+console.log(FCC, fact); 
