1d79f39353e32918b9763e9e9c169bd78dcd67d5
Renessa Cerqueira <renessasantos123@gmail.com>
2023-11-14 17:03:13 -0300

    Atualize o código para que ele use apenas a palavra-chave let.

--- basic-javascript/explore-differences-between-the-var-and-let-keywords.js
+++ basic-javascript/explore-differences-between-the-var-and-let-keywords.js
@@ -1,2 +1,16 @@
+// Explore as diferenças entre as palavras-chave var e let
+
+// 1. Escopo: var: Tem escopo de função, o que significa que a variável pode ser vista em toda a função onde é declarada. let: Tem escopo de bloco, então a variável só pode ser vista dentro do bloco (entre chaves) onde é declarada.
+
+// 2. Içamento (Hoisting): var: É "içada" para o topo da função ou escopo global, podendo ser usada antes de ser declarada. let: Também é içada, mas não pode ser usada antes de ser declarada, evitando comportamentos inesperados.
+
+// 3. Reatribuição: var e let: Ambas podem ser reatribuídas a novos valores.
+
+// Diferentemente de var, quando você usa let, uma variável com o mesmo nome só pode ser declarada uma vez.
+
+// Resumindo, use let em vez de var na maioria dos casos para evitar problemas relacionados ao escopo e ao içamento, tornando seu código mais seguro e fácil de entender.
+
+// Atualize o código para que ele use apenas a palavra-chave let.
+
 let catName = "Oliver";
 let catSound = "Meow!";
